f260440bb53806abf1b2b93cd39fe5023174b2d1
Oleg Pulkin <oleghcp@gmail.com>
2020-03-31 23:15:33 +0300

    some new id generators

---
+++ Code/Runtime/UnityUtility/IdGenerating/NegativeIntIdGenerator.cs
@@ -0,0 +1,24 @@
+﻿namespace UU.IdGenerating
+{
+    public class NegativeIntIdGenerator : IdGenerator<int>
+    {
+        private int m_lastId;
+
+        public NegativeIntIdGenerator() { }
+
+        public NegativeIntIdGenerator(int startId)
+        {
+            m_lastId = startId;
+        }
+
+        public int LastID
+        {
+            get { return m_lastId; }
+        }
+
+        public int GetNewId()
+        {
+            return --m_lastId;
+        }
+    }
+}
---
+++ Code/Runtime/UnityUtility/IdGenerating/NegativeLongIdGenerator.cs
@@ -0,0 +1,24 @@
+﻿namespace UU.IdGenerating
+{
+    public class NegativeLongIdGenerator
+    {
+        private long m_lastId;
+
+        public NegativeLongIdGenerator() { }
+
+        public NegativeLongIdGenerator(long startId)
+        {
+            m_lastId = startId;
+        }
+
+        public long LastID
+        {
+            get { return m_lastId; }
+        }
+
+        public long GetNewId()
+        {
+            return --m_lastId;
+        }
+    }
+}
