c6ee9a1211cf65190bc972066b2058895426daf4
Oleg Pulkin <oleghcp@gmail.com>
2020-04-05 00:21:22 +0300

    async system settings blank gui

---
+++ Code/Editor/Async/AsyncSettingsIMGUIRegister.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+
+internal static class AsyncSettingsIMGUIRegister
+{
+    public const string ASYNC_SYSTEM_SETTINGS_NAME = "Async System Settings";
+
+    [SettingsProvider]
+    public static SettingsProvider CreateMyCustomSettingsProvider()
+    {
+        return new SettingsProvider("Project/" + ASYNC_SYSTEM_SETTINGS_NAME, SettingsScope.Project)
+        {
+            guiHandler = searchContext =>
+            {
+                //var settings = MyCustomSettings.GetSerializedSettings();
+                //EditorGUILayout.PropertyField(settings.FindProperty("m_Number"), new GUIContent("My Number"));
+                //EditorGUILayout.PropertyField(settings.FindProperty("m_SomeString"), new GUIContent("My String"));
+                EditorGUILayout.HelpBox("Blank gui for Async settings.", MessageType.Info);
+            },
+
+            //keywords = new HashSet<string>(new[] { "Number", "Some String" })
+        };
+    }
+}
---
+++ Code/Editor/Async/AsyncSystemSettingsEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityUtility.Async;
+
+namespace UnityUtilityEditor.Async
+{
+    [CustomEditor(typeof(AsyncSystemSettings))]
+    public class AsyncSystemSettingsEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.Space();
+            EditorGUILayout.HelpBox($"Don't touch this asset. Use {AsyncSettingsIMGUIRegister.ASYNC_SYSTEM_SETTINGS_NAME} in Project Settings instead.",
+                                    MessageType.Warning);
+        }
+    }
+}
