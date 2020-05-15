c2ff435c92e990cb556417dcbdbfa1a4bd386b44
Oleg Pulkin <oleghcp@gmail.com>
2020-05-16 00:16:00 +0300

    PathUtility upd

--- Code/Runtime/System/IO/PathUtility.cs
+++ Code/Runtime/System/IO/PathUtility.cs
@@ -4,24 +4,36 @@ namespace System.IO
 {
     public static class PathUtility
     {
-        public static string GetParentPath(string path, int steps = 1)
+        public static string GetName(string path)
         {
-            bool separatorChecker(char ch)
+            int separatorIndex = -1;
+            bool terminator = true;
+
+            for (int i = path.Length - 1; i >= 0; i--)
             {
-                return ch == Path.DirectorySeparatorChar || ch == Path.AltDirectorySeparatorChar;
+                if (f_isSeparator(path[i]))
+                {
+                    if (terminator)
+                        continue;
+
+                    separatorIndex = i;
+                    break;
+                }
+                else
+                {
+                    terminator = false;
+                }
             }
 
-            return f_getParentPath(path, steps, separatorChecker);
+            int startIndex = separatorIndex + 1;
+            int length = f_isSeparator(path[path.Length - 1]) ?
+                         path.Length - startIndex - 1 :
+                         path.Length - startIndex;
+
+            return path.Substring(startIndex, length);
         }
 
-        public static string GetParentPath(string path, char separator, int steps = 1)
-        {
-            return f_getParentPath(path, steps, ch => ch == separator);
-        }
-
-        // -- //
-
-        private static string f_getParentPath(string path, int steps, Func<char, bool> separatorChecker)
+        public static string GetParentPath(string path, int steps = 1)
         {
             string parent = path;
             int maxLength = path.Length;
@@ -31,19 +43,27 @@ namespace System.IO
                 bool terminator = true;
                 for (int j = maxLength - 1; j >= 0; j--)
                 {
-                    if (separatorChecker(parent[j]))
+                    if (f_isSeparator(parent[j]))
                     {
-                        if (!terminator)
-                        {
-                            maxLength = j;
-                            break;
-                        }
+                        if (terminator)
+                            continue;
+
+                        maxLength = j;
+                        break;
                     }
-                    else { terminator = false; }
+                    else
+                    {
+                        terminator = false;
+                    }
                 }
             }
 
             return parent.Substring(0, maxLength);
+        }
+
+        private static bool f_isSeparator(char ch)
+        {
+            return ch == Path.DirectorySeparatorChar || ch == Path.AltDirectorySeparatorChar;
         }
     }
 
