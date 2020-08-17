d87d50a46bc4111dd55cf468ba45405be4e9ffd8
Oleg Pulkin <oleghcp@gmail.com>
2020-08-17 18:17:03 +0300

    rename TimeCounter to IntervalChecker, refactoring

---
+++ Code/Runtime/UnityUtility/IntervalChecker.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Tools;
+using UnityEngine;
+using UnityUtility.MathExt;
+
+namespace UnityUtility
+{
+    [Serializable]
+    public struct IntervalChecker
+    {
+        [SerializeField]
+        private float m_interval;
+        [SerializeField]
+        private float m_currentValue;
+
+        public float Interval
+        {
+            get { return m_interval; }
+            set
+            {
+                if (value <= 0f)
+                    throw Errors.ZeroParameter(nameof(value));
+
+                m_interval = value;
+            }
+        }
+
+        public float CurrentValue
+        {
+            get { return m_currentValue; }
+            set { m_currentValue = value.CutAfter(m_interval); }
+        }
+
+        public float Ratio
+        {
+            get { return m_currentValue / m_interval; }
+            set { m_currentValue = m_interval * value.Saturate(); }
+        }
+
+        public float Shortage
+        {
+            get { return (m_interval - m_currentValue).CutBefore(0f); }
+        }
+
+        public IntervalChecker(float interval)
+        {
+            if (interval <= 0f)
+                throw Errors.ZeroParameter(nameof(interval));
+
+            m_interval = interval;
+            m_currentValue = 0f;
+        }
+
+        public IntervalChecker(float interval, float startValue)
+        {
+            if (interval <= 0f)
+                throw Errors.ZeroParameter(nameof(interval));
+
+            m_interval = interval;
+            m_currentValue = startValue.CutAfter(interval);
+        }
+
+        public bool HardCheckDelta(float deltaValue)
+        {
+            if (deltaValue < 0f)
+                Errors.NegativeParameter(nameof(deltaValue));
+
+            return HardCheck(m_currentValue + deltaValue);
+        }
+
+        public bool HardCheck(float newValue)
+        {
+            if (newValue >= m_interval)
+            {
+                m_currentValue = 0f;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool SmoothCheckDelta(float deltaValue)
+        {
+            if (deltaValue < 0f)
+                Errors.NegativeParameter(nameof(deltaValue));
+
+            return SmoothCheck(m_currentValue + deltaValue);
+        }
+
+        public bool SmoothCheck(float newValue)
+        {
+            if (newValue >= m_interval)
+            {
+                m_currentValue = newValue - m_interval;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
