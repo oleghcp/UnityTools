74a579b331d695c49e07a228eff2755ef045c74b
Oleg Pulkin <oleghcp@gmail.com>
2020-09-06 23:20:51 +0300

    upd DisableEditingDrawer

--- Code/Editor/UnityUtilityEditor/Drawers/DisableEditingDrawer.cs
+++ Code/Editor/UnityUtilityEditor/Drawers/DisableEditingDrawer.cs
@@ -10,8 +10,13 @@ namespace UnityUtilityEditor.Drawers
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             GUI.enabled = false;
-            EditorGUI.PropertyField(position, property, label);
+            EditorGUI.PropertyField(position, property, label, true);
             GUI.enabled = true;
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, true) + EditorGUIUtility.standardVerticalSpacing;
         }
     }
 }
