16ea9b876ca04a1ce9259acb44d4d4e73e5d8d0d
Oleg Pulkin <oleghcp@gmail.com>
2020-12-18 18:07:19 +0300

    UnityEventExtensions

---
+++ Code/Runtime/UnityEngine/Events/UnityEventExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityObject = UnityEngine.Object;
+
+namespace UnityEngine.Events
+{
+    public static class UnityEventExtensions
+    {
+        public static IEnumerable<UnityObject> EnumerateListeners(this UnityEventBase self)
+        {
+            int count = self.GetPersistentEventCount();
+
+            for (int i = 0; i < count; i++)
+            {
+                yield return self.GetPersistentTarget(i);
+            }
+        }
+    }
+}
