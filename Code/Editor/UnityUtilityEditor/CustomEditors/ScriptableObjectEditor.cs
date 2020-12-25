21e557f6154b55811b4a426840d772ec18fe424b
Oleg Pulkin <oleghcp@gmail.com>
2020-12-25 20:50:12 +0300

    edit script button for ScriptableObject assets

---
+++ Code/Editor/UnityUtilityEditor/CustomEditors/ScriptableObjectEditor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UnityUtilityEditor.CustomEditors
+{
+    [CustomEditor(typeof(ScriptableObject), true)]
+    public class ScriptableObjectEditor : Editor
+    {
+        protected override void OnHeaderGUI()
+        {
+            EditorGUILayout.BeginHorizontal();
+            {
+                base.OnHeaderGUI();
+                EditorGUILayout.BeginVertical();
+                {
+                    if (GUILayout.Button("Edit\nScript", GUILayout.MinHeight(50f)))
+                    {
+                        var prop = serializedObject.FindProperty("m_Script");
+                        var filePath = AssetDatabase.GetAssetPath(prop.objectReferenceValue);
+                        System.Diagnostics.Process.Start("devenv", "/edit " + filePath);
+                    }
+                }
+                EditorGUILayout.EndVertical();
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        protected override bool ShouldHideOpenButton()
+        {
+            return true;
+        }
+    }
+}
