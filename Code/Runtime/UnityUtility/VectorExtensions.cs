94aaed4e829d8d94d8a57fa8ab51bf99542fb5a9
Oleg Pulkin <oleghcp@gmail.com>
2021-01-06 05:29:18 +0300

    ToMinMaxTuple extensions for vector2

---
+++ Code/Runtime/UnityUtility/VectorExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace UnityUtility
+{
+    public static class VectorExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static (float min, float max) ToMinMaxTuple(this in Vector2 self)
+        {
+            return (self.x, self.y);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static (int min, int max) ToMinMaxTuple(this in Vector2Int self)
+        {
+            return (self.x, self.y);
+        }
+    }
+}
