5356437ebce13c787bef5a7df3823aa83a7097f5
Oleg Pulkin <oleghcp@gmail.com>
2021-01-10 10:30:23 +0300

    upd CameraScaler

--- Code/Runtime/UnityUtility/VerticalCameraScaler.cs
+++ Code/Runtime/UnityUtility/VerticalCameraScaler.cs
@@ -3,19 +3,16 @@ using UnityEngine;
 
 namespace UnityUtility
 {
-    [ExecuteInEditMode]
+    [ExecuteAlways]
     [RequireComponent(typeof(Camera))]
     [DisallowMultipleComponent]
-    public sealed class CameraScaler : MonoBehaviour
+    public sealed class VerticalCameraScaler : MonoBehaviour
     {
         [SerializeField, HideInInspector]
         private Camera _camera;
         [SerializeField]
-        private float _targetHeight = 3f;
-        [SerializeField]
-        private float _targetWidth = 4f;
+        private float _horizontalSize = 4f;
 
-        private float _targetRatio;
         private float _aspectRatio;
 
         private void Awake()
@@ -27,35 +24,23 @@ namespace UnityUtility
         {
             float newRatio = f_getCurrentRatio();
 
-            if (newRatio < _targetRatio)
-            {
-                if (_camera.orthographicSize != _targetHeight)
-                    _camera.orthographicSize = _targetHeight;
-
-                return;
-            }
-
             if (_aspectRatio != newRatio)
             {
-                _camera.orthographicSize = _targetWidth * newRatio;
+                _camera.orthographicSize = _horizontalSize * newRatio;
                 _aspectRatio = newRatio;
             }
         }
 
         private void OnValidate()
         {
-            _camera = GetComponent<Camera>();
             f_initialize();
         }
 
         private void f_initialize()
         {
-            _targetRatio = _targetHeight / _targetWidth;
+            _camera = GetComponent<Camera>();
             _aspectRatio = f_getCurrentRatio();
-            if (_aspectRatio < _targetRatio)
-                _camera.orthographicSize = _targetHeight;
-            else
-                _camera.orthographicSize = _targetWidth * _aspectRatio;
+            _camera.orthographicSize = _horizontalSize * _aspectRatio;
 
         }
 
