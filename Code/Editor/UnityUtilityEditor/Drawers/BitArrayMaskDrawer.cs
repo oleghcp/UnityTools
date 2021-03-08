a44240a2fc0db123a033ba3b570a7690798f5955
Oleg Pulkin <oleghcp@gmail.com>
2021-03-08 13:29:39 +0300

    upd

--- Code/Editor/UnityUtilityEditor/Drawers/BitArrayMaskDrawer.cs
+++ Code/Editor/UnityUtilityEditor/Drawers/BitArrayMaskDrawer.cs
@@ -18,22 +18,22 @@ namespace UnityUtilityEditor.Drawers
     [CustomPropertyDrawer(typeof(BitArrayMask))]
     internal class BitArrayMaskDrawer : PropertyDrawer
     {
-        private SimpleBitArrayMaskWindow m_win;
+        private SimpleBitArrayMaskWindow _window;
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            Draw(position, label, ref m_win, property);
+            Draw(position, label, ref _window, property);
         }
 
-        internal static void Draw<T>(Rect position, GUIContent label, ref T popup, object popupParam) where T : BitArrayMaskWindow
+        public static void Draw<T>(Rect position, GUIContent label, ref T window, object popupParam) where T : BitArrayMaskWindow
         {
             position.size = new Vector2(position.size.x, EditorGUIUtility.singleLineHeight);
             Rect rect = EditorGUI.PrefixLabel(position, label);
             if (GUI.Button(rect, "Edit values"))
-                if (popup == null)
-                    (popup = BitArrayMaskWindow.Create<T>()).SetUp(popupParam);
+                if (window == null)
+                    (window = BitArrayMaskWindow.Create<T>()).SetUp(popupParam);
                 else
-                    popup.Focus();
+                    window.Focus();
         }
     }
 }
