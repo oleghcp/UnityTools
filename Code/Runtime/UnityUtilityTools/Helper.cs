62acd7a863a11a917bd8b9edceaf17ba8b74ecfc
Oleg Pulkin <oleghcp@gmail.com>
2021-04-27 07:47:35 +0300

    remove clone function

--- Code/Runtime/UnityUtilityTools/Helper.cs
+++ Code/Runtime/UnityUtilityTools/Helper.cs
@@ -1,36 +1,8 @@
 ﻿using System;
-using System.Reflection;
-using System.Runtime.CompilerServices;
 using UnityUtility;
 
 namespace UnityUtilityTools
 {
-    public enum MemberCloneOption
-    {
-        Clone,
-        Copy,
-        Ignore
-    }
-
-    public enum CloneOption
-    {
-        Full,
-        AsICloneable,
-        FieldsAsICloneable,
-        AsICloneableOrFieldsAsICloneable
-    }
-
-    [AttributeUsage(AttributeTargets.Field)]
-    public sealed class CloneableAttribute : Attribute
-    {
-        public readonly MemberCloneOption Option;
-
-        public CloneableAttribute(MemberCloneOption option)
-        {
-            Option = option;
-        }
-    }
-
     public static class Helper
     {
         //The copy of internal System.Numerics.Hashing.HashHelpers
@@ -53,99 +25,6 @@ namespace UnityUtilityTools
         public static int GetHashCode(int hc0, int hc1, int hc2, int hc3)
         {
             return hc0 ^ hc1 << 2 ^ hc2 >> 2 ^ hc3 >> 1;
-        }
-
-        /// <summary>
-        /// Clones any object with public or private default constructor.
-        /// </summary>
-        public static object CloneObject(object sourceObj, CloneOption option = CloneOption.Full)
-        {
-            if (sourceObj is null)
-                return null;
-
-            if (option == CloneOption.AsICloneable && sourceObj is ICloneable cloneable)
-                return cloneable.Clone();
-
-            if (sourceObj is Pointer)
-                return sourceObj;
-
-            Type type = sourceObj.GetType();
-
-            if (type.IsArray)
-            {
-                Array sourceArray = sourceObj as Array;
-                Array destArray = Array.CreateInstance(type.GetElementType(), sourceArray.Length);
-
-                for (int i = 0; i < sourceArray.Length; i++)
-                {
-                    object sourceValue = sourceArray.GetValue(i);
-                    if (sourceValue == null) { continue; }
-
-                    destArray.SetValue(CloneObject(sourceValue, ConvertToFieldOption(option)), i);
-                }
-
-                return destArray;
-            }
-
-            object destObj = Activator.CreateInstance(type, true);
-
-            FieldInfo[] fields = type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-
-            for (int i = 0; i < fields.Length; i++)
-            {
-                object sourceFieldValue = fields[i].GetValue(sourceObj);
-
-                if (sourceFieldValue == null)
-                    continue;
-
-                CloneableAttribute attribute = Attribute.GetCustomAttribute(fields[i], typeof(CloneableAttribute)) as CloneableAttribute;
-                MemberCloneOption cloneOption = attribute == null ? MemberCloneOption.Clone : attribute.Option;
-
-                if (cloneOption == MemberCloneOption.Ignore)
-                    continue;
-
-                if (cloneOption == MemberCloneOption.Copy)
-                {
-                    fields[i].SetValue(destObj, sourceFieldValue);
-                    continue;
-                }
-
-                bool asICloneable = option == CloneOption.FieldsAsICloneable || option == CloneOption.AsICloneableOrFieldsAsICloneable;
-
-                if (asICloneable && sourceFieldValue is ICloneable sourceFieldValueCloneable)
-                {
-                    fields[i].SetValue(destObj, sourceFieldValueCloneable.Clone());
-                    continue;
-                }
-
-                fields[i].SetValue(destObj, CloneObject(sourceFieldValue, ConvertToFieldOption(option)));
-            }
-
-            return destObj;
-        }
-
-        /// <summary>
-        /// Clones any object with public or private default constructor.
-        /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static T Clone<T>(T sourceObj, CloneOption option = CloneOption.Full)
-        {
-            return (T)CloneObject(sourceObj, option);
-        }
-
-        // -- //
-
-        private static CloneOption ConvertToFieldOption(CloneOption option)
-        {
-            switch (option)
-            {
-                case CloneOption.Full:
-                case CloneOption.AsICloneable:
-                    return CloneOption.Full;
-
-                default:
-                    return CloneOption.AsICloneableOrFieldsAsICloneable;
-            }
         }
 
         internal static string CutAssemblyQualifiedName(string assemblyQualifiedName)
