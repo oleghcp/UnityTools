4028b1aa49869d4f5b57d89b7704ac8d4a744323
Oleg Pulkin <oleghcp@gmail.com>
2021-05-10 00:11:26 +0300

    upd CameraFitter

---
+++ Code/Editor/UnityUtilityEditor/Inspectors/CameraFitterEditor.cs
@@ -0,0 +1,46 @@
+﻿using UnityEditor;
+using UnityUtility;
+
+namespace UnityUtilityEditor.Inspectors
+{
+    [CustomEditor(typeof(CameraFitter))]
+    internal class CameraFitterEditor : Editor<CameraFitter>
+    {
+        private SerializedProperty _mode;
+        private SerializedProperty _vertical;
+        private SerializedProperty _horizontal;
+
+        private void OnEnable()
+        {
+            _mode = serializedObject.FindProperty(CameraFitter.ModeFieldName);
+            _vertical = serializedObject.FindProperty(CameraFitter.VerticalFieldName);
+            _horizontal = serializedObject.FindProperty(CameraFitter.HorizontalFieldName);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            Draw();
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void Draw()
+        {
+            EditorGUILayout.PropertyField(_mode);
+
+            if (_mode.enumValueIndex == ((int)AspectMode.FixedHeight))
+                return;
+
+            bool ortho = target.Camera.orthographic;
+
+            if (_mode.enumValueIndex == ((int)AspectMode.FixedWidth))
+            {
+                EditorGUILayout.PropertyField(_horizontal, EditorGuiUtility.TempContent(ortho ? "Horizontal Size" : "Horizontal Fov"));
+                return;
+            }
+
+            EditorGUILayout.PropertyField(_horizontal, EditorGuiUtility.TempContent(ortho ? "Target Horizontal Size" : "Target Horizontal Fov"));
+            EditorGUILayout.PropertyField(_vertical, EditorGuiUtility.TempContent(ortho ? "Target Vertical Size" : "Target Vertical Fov"));
+        }
+    }
+}
---
+++ Code/Runtime/UnityUtility/CameraFitter.cs
@@ -0,0 +1,138 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace UnityUtility
+{
+    public enum AspectMode
+    {
+        FixedHeight,
+        FixedWidth,
+        EnvelopeAspect,
+    }
+
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(Camera))]
+    [DisallowMultipleComponent]
+    public sealed class CameraFitter : MonoBehaviour
+    {
+        [SerializeField, HideInInspector]
+        private Camera _camera;
+        [SerializeField]
+        private AspectMode _aspectMode;
+        [SerializeField]
+        private float _targetVertical;
+        [SerializeField]
+        private float _targetHorizontal;
+
+        private float _currentAspect;
+
+#if UNITY_EDITOR
+        internal static string ModeFieldName => nameof(_aspectMode);
+        internal static string VerticalFieldName => nameof(_targetVertical);
+        internal static string HorizontalFieldName => nameof(_targetHorizontal);
+#endif
+
+        public Camera Camera => _camera;
+        public AspectMode AspectMode
+        {
+            get => _aspectMode;
+            set
+            {
+                if (_aspectMode == value)
+                    return;
+
+                _aspectMode = value;
+
+                if (_camera.orthographic)
+                    OrthoInit();
+                else
+                    PerspInit();
+            }
+        }
+
+        private void Awake()
+        {
+            if (_camera == null)
+                _camera = GetComponent<Camera>();
+
+            if (_aspectMode == AspectMode.FixedHeight)
+                return;
+
+            _currentAspect = GetCurrentRatio();
+
+            if (_camera.orthographic)
+                OrthoInit();
+            else
+                PerspInit();
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (_camera.orthographic)
+                OrthoInit();
+            else
+                PerspInit();
+        }
+#endif
+
+        private void LateUpdate()
+        {
+            if (_aspectMode == AspectMode.FixedHeight)
+                return;
+
+            float newRatio = GetCurrentRatio();
+
+            if (_currentAspect != newRatio)
+            {
+                _currentAspect = newRatio;
+
+                if (_camera.orthographic)
+                    OrthoInit();
+                else
+                    PerspInit();
+            }
+        }
+
+        private void OrthoInit()
+        {
+            if (_aspectMode == AspectMode.FixedWidth)
+            {
+                _camera.orthographicSize = _targetHorizontal * _currentAspect;
+                return;
+            }
+
+            float targetRatio = _targetVertical / _targetHorizontal;
+
+            if (targetRatio >= _currentAspect)
+                _camera.orthographicSize = _targetVertical;
+            else
+                _camera.orthographicSize = _targetHorizontal * _currentAspect;
+        }
+
+        private void PerspInit()
+        {
+            if (_aspectMode == AspectMode.FixedWidth)
+            {
+                _camera.fieldOfView = ScreenUtility.GetAspectAngle(_targetHorizontal, _currentAspect);
+                return;
+            }
+
+            float vTan = ScreenUtility.HalfFovTan(_targetVertical);
+            float hTan = ScreenUtility.HalfFovTan(_targetHorizontal);
+
+            float targetRatio = vTan / hTan;
+
+            if (targetRatio >= _currentAspect)
+                _camera.fieldOfView = _targetVertical;
+            else
+                _camera.fieldOfView = ScreenUtility.GetAspectAngle(_targetHorizontal, _currentAspect);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static float GetCurrentRatio()
+        {
+            return (float)Screen.height / Screen.width;
+        }
+    }
+}
