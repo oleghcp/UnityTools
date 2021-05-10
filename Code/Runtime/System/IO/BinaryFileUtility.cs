e4524eb4c0ef7b47b0bdd714e6b443aef0d1c8e3
Oleg Pulkin <oleghcp@gmail.com>
2021-05-10 23:17:31 +0300

    BinaryFileUtility

---
+++ Code/Runtime/System/IO/BinaryFileUtility.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace System.IO
+{
+    public class BinaryFileUtility
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T Load<T>(string path)
+        {
+            return (T)Load(path);
+        }
+
+        public static object Load(string path)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            using (Stream stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                return formatter.Deserialize(stream);
+            }
+        }
+
+        public static void Save(string path, object obj)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            using (Stream stream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                formatter.Serialize(stream, obj);
+            }
+        }
+    }
+}
