f954534e5599c19d3f24ee8309b887ba179ff5af
Oleg Pulkin <oleghcp@gmail.com>
2021-05-10 20:51:00 +0300

    upd TransformEditor

--- Code/Editor/UnityUtilityEditor/Inspectors/TransformEditor.cs
+++ Code/Editor/UnityUtilityEditor/Inspectors/TransformEditor.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEditor;
 using UnityEngine;
+using UnityUtility;
 using UnityUtility.MathExt;
 
 #if !UNITY_2019_3_OR_NEWER
@@ -17,7 +18,9 @@ namespace UnityUtilityEditor.Inspectors
         private const float BTN_WIDTH = 40f;
         private const float LABEL_WIDTH = 40f;
 
-        private static readonly string[] _toolbarNames = new string[] { "Local", "World" };
+        private string[] _toolbarNames = new string[] { "Local", "World" };
+        private string _pivotModeWarning = $"{PivotMode.Center} Pivot Mode Enabled";
+        private string _pivotRotationWarning = $"{PivotRotation.Global} Pivot Rotation Enabled";
 
         private static bool _world;
 
@@ -29,7 +32,7 @@ namespace UnityUtilityEditor.Inspectors
         private const float TOGGLE_WIDTH = 15f;
         private static bool _grid;
         private static float _snapStep = 1f;
-        private readonly static Vector3[] s_axes = { Vector3.forward, Vector3.right, Vector3.up };
+        private Vector3[] _axes = { Vector3.forward, Vector3.right, Vector3.up };
         private Handles _handles;
 #endif
 
@@ -38,6 +41,61 @@ namespace UnityUtilityEditor.Inspectors
             _posProp = serializedObject.FindProperty("m_LocalPosition");
             _rotProp = serializedObject.FindProperty("m_LocalRotation");
             _sclProp = serializedObject.FindProperty("m_LocalScale");
+
+            Tools.pivotModeChanged += Repaint;
+            Tools.pivotRotationChanged += Repaint;
+        }
+
+        private void OnDestroy()
+        {
+            Tools.pivotModeChanged -= Repaint;
+            Tools.pivotRotationChanged -= Repaint;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            GUILayout.Space(SPACE);
+
+            bool hasParent = target.parent;
+
+            GUI.enabled = hasParent;
+            _world = GUILayout.Toolbar((hasParent && _world).ToInt(), _toolbarNames).ToBool();
+            GUI.enabled = true;
+
+            GUILayout.Space(SPACE);
+
+            if (_world)
+            {
+                if (DrawLineForWorld("[ Pos ]", target.position, out Vector3 pos))
+                    target.position = pos;
+
+                if (DrawLineForWorld("[ Rot ]", target.eulerAngles, out Vector3 rot))
+                    target.eulerAngles = rot;
+
+                DrawLineForWorld("[ Scl ]", target.lossyScale, out _, true);
+            }
+            else
+            {
+                serializedObject.Update();
+
+                _posProp.vector3Value = DrawLineForLocal("Pos", _posProp.vector3Value);
+                _rotProp.quaternionValue = DrawLineForLocal("Rot", _rotProp.quaternionValue.eulerAngles).ToRotation();
+                _sclProp.vector3Value = DrawLineForLocal("Scl", _sclProp.vector3Value, Vector3.one);
+
+                serializedObject.ApplyModifiedProperties();
+            }
+
+            GUILayout.Space(SPACE);
+
+            GUI.color = Colours.Yellow;
+
+            if (Tools.pivotMode != PivotMode.Pivot)
+                EditorGUILayout.HelpBox(_pivotModeWarning, MessageType.None);
+
+            if (Tools.pivotRotation != PivotRotation.Local)
+                EditorGUILayout.HelpBox(_pivotRotationWarning, MessageType.None);
+
+            GUI.color = Colours.White;
         }
 
 #if !UNITY_2019_3_OR_NEWER
@@ -78,7 +136,7 @@ namespace UnityUtilityEditor.Inspectors
                 Vector3 n = -(_handles ?? (_handles = new Handles())).currentCamera.transform.forward;
                 const float CIRCLE_SIZE = 0.03f;
 
-                int index = s_axes.IndexOfMax(itm => (Vector3.Angle(itm, n) - 90).Abs());
+                int index = _axes.IndexOfMax(itm => (Vector3.Angle(itm, n) - 90).Abs());
 
                 Vector3 nodePos(int i, int j)
                 {
@@ -107,95 +165,41 @@ namespace UnityUtilityEditor.Inspectors
         }
 #endif
 
-        public override void OnInspectorGUI()
+        private static Vector3 DrawLineForLocal(string label, in Vector3 curValue, in Vector3 defVal = default)
         {
-            bool hasParent = target.parent;
+            Vector3 newValue;
 
-            if (!hasParent)
-                _world = false;
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button(label, GUILayout.Width(BTN_WIDTH), GUILayout.Height(EditorGUIUtility.singleLineHeight)))
+                EditorGUILayout.Vector3Field(GUIContent.none, newValue = defVal);
+            else
+                newValue = EditorGUILayout.Vector3Field(GUIContent.none, curValue);
+            EditorGUILayout.EndHorizontal();
 
-            GUILayout.Space(SPACE);
+            return newValue;
+        }
 
-            GUI.enabled = hasParent;
-            _world = GUILayout.Toolbar(_world.ToInt(), _toolbarNames)
-                               .ToBool();
-            GUI.enabled = true;
-
-            GUILayout.Space(SPACE);
-
-            if (_world)
+        private bool DrawLineForWorld(string label, in Vector3 curValue, out Vector3 newValue, bool locked = false)
+        {
+            bool changed = false;
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField(label, GUILayout.Width(LABEL_WIDTH));
+            if (locked)
             {
-                bool drawLine(string label, in Vector3 curValue, out Vector3 newValue, bool locked = false)
-                {
-                    bool changed;
-                    EditorGUILayout.BeginHorizontal();
-                    EditorGUILayout.LabelField(label, GUILayout.Width(LABEL_WIDTH));
-                    if (locked)
-                    {
-                        GUI.enabled = false;
-                        EditorGUILayout.Vector3Field(GUIContent.none, curValue);
-                        GUI.enabled = true;
-                        changed = false;
-                        newValue = default;
-                    }
-                    else
-                    {
-                        EditorGUI.BeginChangeCheck();
-                        newValue = EditorGUILayout.Vector3Field(GUIContent.none, curValue);
-                        if (changed = EditorGUI.EndChangeCheck())
-                            Undo.RecordObject(target, UNDO_NAME);
-                    }
-                    EditorGUILayout.EndHorizontal();
-                    return changed;
-                }
-
-                if (drawLine("[ Pos ]", target.position, out Vector3 pos)) { target.position = pos; }
-                if (drawLine("[ Rot ]", target.eulerAngles, out Vector3 rot)) { target.eulerAngles = rot; }
-                drawLine("[ Scl ]", target.lossyScale, out _, true);
+                GUI.enabled = false;
+                EditorGUILayout.Vector3Field(GUIContent.none, curValue);
+                GUI.enabled = true;
+                newValue = default;
             }
             else
             {
-                bool changed = false;
-
-                bool drawLine(string label, in Vector3 curValue, out Vector3 newValue, in Vector3 defVal = default)
-                {
-                    EditorGUI.BeginChangeCheck();
-                    EditorGUILayout.BeginHorizontal();
-                    if (GUILayout.Button(label, GUILayout.Width(BTN_WIDTH), GUILayout.Height(EditorGUIUtility.singleLineHeight))) { newValue = defVal; }
-                    else { newValue = EditorGUILayout.Vector3Field(GUIContent.none, curValue); }
-                    EditorGUILayout.EndHorizontal();
-                    bool ch = EditorGUI.EndChangeCheck();
-                    changed |= ch;
-                    return ch;
-                }
-
-                if (drawLine("Pos", _posProp.vector3Value, out Vector3 pos)) { _posProp.vector3Value = pos; }
-                if (drawLine("Rot", _rotProp.quaternionValue.eulerAngles, out Vector3 rot)) { _rotProp.quaternionValue = rot.ToRotation(); }
-                if (drawLine("Scl", _sclProp.vector3Value, out Vector3 scl, Vector3.one)) { _sclProp.vector3Value = scl; }
-
-                if (changed)
-                    serializedObject.ApplyModifiedProperties();
+                EditorGUI.BeginChangeCheck();
+                newValue = EditorGUILayout.Vector3Field(GUIContent.none, curValue);
+                if (changed = EditorGUI.EndChangeCheck())
+                    Undo.RecordObject(target, UNDO_NAME);
             }
-
-            GUILayout.Space(SPACE);
-
-#if !UNITY_2019_3_OR_NEWER
-            EditorGUILayout.BeginHorizontal();
-            bool gridNewVal = GUILayout.Toggle(_grid, string.Empty, GUILayout.Width(TOGGLE_WIDTH));
-            if (_grid != gridNewVal)
-            {
-                _grid = gridNewVal;
-                Tools.hidden = gridNewVal;
-                EditorWindow.GetWindow<SceneView>()
-                            .Repaint();
-            }
-            EditorGUI.BeginDisabledGroup(!_grid);
-            _snapStep = EditorGUILayout.FloatField("Grid Snap Step", _snapStep);
-            EditorGUI.EndDisabledGroup();
             EditorGUILayout.EndHorizontal();
-
-            GUILayout.Space(SPACE);
-#endif
+            return changed;
         }
     }
 }
