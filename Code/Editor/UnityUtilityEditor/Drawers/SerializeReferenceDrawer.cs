beeeec33181df5e73ce52e34a7a2864225a6dcaf
Oleg Pulkin <oleghcp@gmail.com>
2021-10-07 08:26:45 +0300

    upd SerializeReferenceDrawer

--- Code/Editor/UnityUtilityEditor/Drawers/SerializeReferenceDrawer.cs
+++ Code/Editor/UnityUtilityEditor/Drawers/SerializeReferenceDrawer.cs
@@ -29,15 +29,13 @@ namespace UnityUtilityEditor.Drawers
                 return;
             }
 
-            label = EditorGUI.BeginProperty(position, label, property);
             DrawContent(position, property);
             EditorGUI.PropertyField(position, property, label, true);
-            EditorGUI.EndProperty();
         }
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
-            return EditorGUI.GetPropertyHeight(property, label);
+            return EditorGUI.GetPropertyHeight(property, label, true);
         }
 
         protected abstract void DrawContent(Rect position, SerializedProperty property);
