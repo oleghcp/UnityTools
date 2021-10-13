2163beb8bf1dcf972bd3afacbe8303c826b1845f
Oleg Pulkin <oleghcp@gmail.com>
2021-10-13 12:16:46 +0300

    remove unused stuff from EnumUtility

--- Code/Runtime/System/EnumUtility.cs
+++ Code/Runtime/System/EnumUtility.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace System
+﻿namespace System
 {
     public static class Enum<TEnum> where TEnum : Enum
     {
@@ -8,31 +6,7 @@ namespace System
 
         static Enum()
         {
-            Count = GetNames().Length;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static string[] GetNames()
-        {
-            return Enum.GetNames(typeof(TEnum));
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static TEnum[] GetValues()
-        {
-            return Enum.GetValues(typeof(TEnum)) as TEnum[];
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static TEnum Parse(string value)
-        {
-            return (TEnum)Enum.Parse(typeof(TEnum), value);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static TEnum Parse(string value, bool ignoreCase)
-        {
-            return (TEnum)Enum.Parse(typeof(TEnum), value, ignoreCase);
+            Count = Enum.GetNames(typeof(TEnum)).Length;
         }
     }
 }
