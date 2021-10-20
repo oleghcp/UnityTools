15bcda79d61e41bc94ecad31d9a711b67c0746c7
Oleg Pulkin <oleghcp@gmail.com>
2021-10-20 12:31:24 +0300

    upd SortingLayerIDDrawer

--- Code/Editor/UnityUtilityEditor/Drawers/SortingLayerIDDrawer.cs
+++ Code/Editor/UnityUtilityEditor/Drawers/SortingLayerIDDrawer.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using UnityUtility.Inspector;
 using UnityUtility.MathExt;
-using static UnityEngine.SortingLayer;
 
 namespace UnityUtilityEditor.Drawers
 {
@@ -34,25 +32,60 @@ namespace UnityUtilityEditor.Drawers
         public class DrawTool
         {
             private string[] _names;
+            private SortingLayer[] _layers;
 
             public DrawTool()
             {
-                _names = layers.Select(itm => itm.name)
-                                .ToArray();
+                _layers = SortingLayer.layers;
+                _names = new string[_layers.Length + 2];
+
+                for (int i = 0; i < _layers.Length; i++)
+                {
+                    _names[i] = _layers[i].name;
+                }
+
+                _names.FromEnd(0) = "Add Sorting Layer...";
+            }
+
+            public int Draw(int layerId)
+            {
+                return Draw(null, layerId);
             }
 
             public int Draw(string propertyName, int layerId)
             {
-                int index = layers.IndexOf(itm => itm.id == layerId).CutBefore(0);
-                index = EditorGuiLayout.DropDown(propertyName, index, _names);
-                return layers[index].id;
+                int index = GetIndex(layerId);
+                int newIndex = EditorGuiLayout.DropDown(propertyName, index, _names);
+                return CheckIndex(index, newIndex);
             }
 
-            public int Draw(Rect position, string propertyName, int layerId)
+            public int Draw(in Rect position, int layerId)
             {
-                int index = layers.IndexOf(itm => itm.id == layerId).CutBefore(0);
-                index = EditorGui.DropDown(position, propertyName, index, _names);
-                return layers[index].id;
+                return Draw(position, null, layerId);
+            }
+
+            public int Draw(in Rect position, string propertyName, int layerId)
+            {
+                int index = GetIndex(layerId);
+                int newIndex = EditorGui.DropDown(position, propertyName, index, _names);
+                return CheckIndex(index, newIndex);
+            }
+
+            private int CheckIndex(int prevIndex, int newIndex)
+            {
+                if ((uint)newIndex >= (uint)_layers.Length)
+                {
+                    Selection.activeObject = Managers.GetTagManager();
+                    return _layers[prevIndex].id;
+                }
+
+                return _layers[newIndex].id;
+            }
+
+            private int GetIndex(int layerId)
+            {
+                return _layers.IndexOf(itm => itm.id == layerId)
+                              .Clamp(0, _layers.Length - 1);
             }
         }
     }
