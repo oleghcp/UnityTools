929b266b35350390b27614e410297e6c67b89711
Oleg Pulkin <oleghcp@gmail.com>
2022-05-12 17:40:26 +0300

    upd AboutWindow

--- Code/Editor/UnityUtilityEditor/Window/AboutWindow.cs
+++ Code/Editor/UnityUtilityEditor/Window/AboutWindow.cs
@@ -7,7 +7,8 @@ namespace UnityUtilityEditor.Window
     internal class AboutWindow : EditorWindow
     {
         private string _copyright;
-        private string _description;
+        private string _description1;
+        private string _description2 = "Supports Unity 2018 or higher.";
 
         private void OnEnable()
         {
@@ -16,7 +17,7 @@ namespace UnityUtilityEditor.Window
             Assembly assembly = Assembly.Load(nameof(UnityUtility)) ?? Assembly.GetExecutingAssembly();
 
             var descriptionAttribute = assembly.GetCustomAttribute<AssemblyDescriptionAttribute>();
-            _description = descriptionAttribute.Description;
+            _description1 = descriptionAttribute.Description;
 
             var copyrightAttribute = assembly.GetCustomAttribute<AssemblyCopyrightAttribute>();
             _copyright = copyrightAttribute.Copyright;
@@ -26,7 +27,11 @@ namespace UnityUtilityEditor.Window
         {
             GUILayout.Space(10f);
 
-            EditorGuiLayout.CenterLabel(_description);
+            EditorGuiLayout.CenterLabel(_description1);
+            EditorGuiLayout.CenterLabel(_description2);
+
+            EditorGUILayout.Space();
+
             EditorGuiLayout.CenterLabel(_copyright);
         }
     }
