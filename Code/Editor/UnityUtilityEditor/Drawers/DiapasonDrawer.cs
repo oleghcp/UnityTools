4a6ec6478ce1f020dcff44035f0e8c57da20254a
Oleg Pulkin <oleghcp@gmail.com>
2022-06-02 20:34:01 +0300

    upd DiapasonDrawer

--- Code/Editor/UnityUtilityEditor/Drawers/DiapasonDrawer.cs
+++ Code/Editor/UnityUtilityEditor/Drawers/DiapasonDrawer.cs
@@ -16,27 +16,42 @@ namespace UnityUtilityEditor.Drawers
         {
             position = EditorGUI.PrefixLabel(position, label);
 
-            if (attribute.MinValue > attribute.MaxValue)
-            {
-                EditorGUI.LabelField(position, "Max value cannot be less than min value.");
-                return;
-            }
-
             Type type = EditorUtilityExt.GetFieldType(this);
 
             SerializedProperty min = property.FindPropertyRelative("x");
             SerializedProperty max = property.FindPropertyRelative("y");
 
-            if (!type.Is(typeof(Vector2)))
+            if (type.Is(typeof(Vector2)))
             {
-                EditorGUI.LabelField(position, $"Use for {nameof(Vector2)}.");
+                if (attribute.MinValue > attribute.MaxValue)
+                {
+                    EditorGUI.LabelField(position, "Max value cannot be less than min value.");
+                    return;
+                }
+
+                float[] array = new[] { min.floatValue, max.floatValue };
+                EditorGUI.MultiFloatField(position, _floatSubLabels, array);
+                min.floatValue = array[0].Clamp(attribute.MinValue, attribute.MaxValue);
+                max.floatValue = array[1].Clamp(array[0], attribute.MaxValue);
                 return;
             }
 
-            float[] array = new[] { min.floatValue, max.floatValue };
-            EditorGUI.MultiFloatField(position, _floatSubLabels, array);
-            min.floatValue = array[0].Clamp(attribute.MinValue, attribute.MaxValue);
-            max.floatValue = array[1].Clamp(array[0], attribute.MaxValue);
+            if (type.Is(typeof(Vector2Int)))
+            {
+                if (attribute.MinValue >= attribute.MaxValue)
+                {
+                    EditorGUI.LabelField(position, "Max value must be more than min value.");
+                    return;
+                }
+
+                int[] array = new[] { min.intValue, max.intValue };
+                EditorGUI.MultiIntField(position, _intSubLabels, array);
+                min.intValue = array[0].Clamp((int)attribute.MinValue, (int)attribute.MaxValue);
+                max.intValue = array[1].Clamp(array[0] + 1, (int)attribute.MaxValue);
+                return;
+            }
+
+            EditorGUI.LabelField(position, $"Use for {nameof(Vector2)} or {nameof(Vector2Int)}.");
         }
     }
 }
