ca1a2cc86e7be431d5da81af5c9485f548dae01f
Oleg Pulkin <oleghcp@gmail.com>
2022-08-17 20:19:48 +0300

    ReadOnlyCollectionExtensions

---
+++ Code/Runtime/System/Collections/Generic/ReadOnly/ReadOnlyCollectionExtensions.cs
@@ -0,0 +1,185 @@
+﻿using System.Runtime.CompilerServices;
+using UnityUtility;
+using UnityUtilityTools;
+
+namespace System.Collections.Generic.ReadOnly
+{
+    public static class ReadOnlyCollectionExtensions
+    {
+        public static T[] GetSubArray_<T>(this IReadOnlyList<T> self, int startIndex, int length)
+        {
+            T[] subArray = new T[length];
+            for (int i = 0; i < length; i++) { subArray[i] = self[i + startIndex]; }
+            return subArray;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T[] GetSubArray_<T>(this IReadOnlyList<T> self, int startIndex)
+        {
+            return GetSubArray_(self, startIndex, self.Count - startIndex);
+        }
+
+        public static int IndexOf_<T>(this IReadOnlyList<T> self, T item)
+        {
+            EqualityComparer<T> comparer = EqualityComparer<T>.Default;
+
+            for (int i = 0; i < self.Count; i++)
+            {
+                if (comparer.Equals(self[i], item))
+                    return i;
+            }
+
+            return -1;
+        }
+
+        public static int IndexOf_<T>(this IReadOnlyList<T> self, Predicate<T> condition)
+        {
+            for (int i = 0; i < self.Count; i++)
+            {
+                if (condition(self[i]))
+                    return i;
+            }
+
+            return -1;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int IndexOfMin_<TSource, TKey>(this IReadOnlyList<TSource> self, Func<TSource, TKey> selector)
+        {
+            return CollectionUtility.Min(self, selector, out _, out _);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int IndexOfMax_<TSource, TKey>(this IReadOnlyList<TSource> self, Func<TSource, TKey> selector)
+        {
+            return CollectionUtility.Max(self, selector, out _, out _);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int IndexOfMin_<TSource, TKey>(this IReadOnlyList<TSource> self, Func<TSource, TKey> selector, out TKey min)
+        {
+            return CollectionUtility.Min(self, selector, out _, out min);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int IndexOfMax_<TSource, TKey>(this IReadOnlyList<TSource> self, Func<TSource, TKey> selector, out TKey max)
+        {
+            return CollectionUtility.Max(self, selector, out _, out max);
+        }
+
+        public static T GetRandomItem_<T>(this IReadOnlyList<T> self, IRng generator)
+        {
+            int index = generator.Next(self.Count);
+            return self[index];
+        }
+
+        public static T GetRandomItem_<T>(this IReadOnlyList<T> self)
+        {
+            int index = UnityEngine.Random.Range(0, self.Count);
+            return self[index];
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsNullOrEmpty_<T>(this IReadOnlyCollection<T> self)
+        {
+            return self == null || self.Count == 0;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool HasAnyData_<T>(this IReadOnlyCollection<T> self)
+        {
+            return self != null && self.Count > 0;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T FromEnd_<T>(this IReadOnlyList<T> self, int reverseIndex)
+        {
+            return self[self.Count - (reverseIndex + 1)];
+        }
+
+        public static IEnumerable<T> Enumerate_<T>(this IReadOnlyList<T> self, int startIndex, int length)
+        {
+            if ((uint)startIndex >= (uint)self.Count)
+                throw new ArgumentOutOfRangeException(nameof(startIndex));
+
+            if (startIndex + length > self.Count)
+                throw new ArgumentOutOfRangeException(nameof(length));
+
+            for (int i = startIndex; i < length; i++)
+            {
+                yield return self[i];
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IEnumerable<T> Enumerate_<T>(this IReadOnlyList<T> self, int startIndex)
+        {
+            return Enumerate_(self, startIndex, self.Count - startIndex);
+        }
+
+        public static IEnumerable<T> EnumerateBack_<T>(this IReadOnlyList<T> self, int startReverseIndex, int length)
+        {
+            if ((uint)startReverseIndex >= (uint)self.Count)
+                throw new ArgumentOutOfRangeException(nameof(startReverseIndex), "Index was out of range.");
+
+            if (startReverseIndex + length > self.Count)
+                throw new ArgumentOutOfRangeException(nameof(length));
+
+            for (int i = startReverseIndex; i < length; i++)
+            {
+                yield return self.FromEnd_(i);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IEnumerable<T> EnumerateBack_<T>(this IReadOnlyList<T> self, int startReverseIndex)
+        {
+            return Enumerate_(self, startReverseIndex, self.Count - startReverseIndex);
+        }
+
+        public static void CopyTo_<T>(this IReadOnlyList<T> self, Span<T> target) where T : unmanaged
+        {
+            int count = Math.Min(self.Count, target.Length);
+            for (int i = 0; i < count; i++)
+            {
+                target[i] = self[i];
+            }
+        }
+
+        public static void CopyTo_<T>(this IReadOnlyList<T> self, Span<T> target, int index) where T : unmanaged
+        {
+            if ((uint)index >= (uint)target.Length)
+                throw Errors.IndexOutOfRange();
+
+            int count = Math.Min(self.Count, target.Length - index);
+            for (int i = 0; i < count; i++)
+            {
+                target[i + index] = self[i];
+            }
+        }
+
+        public static void ForEach_<T>(this IReadOnlyList<T> self, Action<T> action)
+        {
+            for (int i = 0; i < self.Count; i++)
+                action(self[i]);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool Contains_<T>(this IReadOnlyList<T> self, Predicate<T> condition)
+        {
+            return self.IndexOf_(condition) >= 0;
+        }
+
+        public static void ForEach_<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> self, Action<KeyValuePair<TKey, TValue>> action)
+        {
+            foreach (var item in self)
+                action(item);
+        }
+
+        public static void ForEach_<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> self, Action<TKey, TValue> action)
+        {
+            foreach (var item in self)
+                action(item.Key, item.Value);
+        }
+    }
+}
