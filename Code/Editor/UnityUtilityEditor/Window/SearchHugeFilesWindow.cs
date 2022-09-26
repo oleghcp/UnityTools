4bf08fb4a977fd77f639d9fe803b9fa9741cc9d9
Oleg Pulkin <oleghcp@gmail.com>
2022-09-26 03:08:45 +0300

    upd SearchHugeFilesWindow

--- Code/Editor/UnityUtilityEditor/Window/SearchHugeFilesWindow.cs
+++ Code/Editor/UnityUtilityEditor/Window/SearchHugeFilesWindow.cs
@@ -13,6 +13,12 @@ namespace UnityUtilityEditor.Window
     {
         private const float WIDTH = 100f;
 
+        private readonly GUILayoutOption[] _iconOptions = new[]
+        {
+            GUILayout.Width(EditorGUIUtility.singleLineHeight),
+            GUILayout.Height(EditorGUIUtility.singleLineHeight),
+        };
+
         private readonly string[] _sizeToolbarNames = new string[] { "Bytes", "KiB", "MiB", "GiB" };
         private readonly string[] _sortWays = new string[] { "Path", "Size ▲", "Size ▼" };
         private int _sizeToolbarIndex = 2;
@@ -21,11 +27,11 @@ namespace UnityUtilityEditor.Window
         private Vector2 _scrollPosition;
 
         private long _fileSize = 100L * 1024 * 1024;
-        private (UnityObject asset, string folder, long size)[] _result;
+        private Container[] _result;
 
         private void OnEnable()
         {
-            minSize = new Vector2(550f, 200f);
+            minSize = new Vector2(600f, 200f);
         }
 
         public static void Create()
@@ -47,11 +53,13 @@ namespace UnityUtilityEditor.Window
 
                 GUILayout.FlexibleSpace();
 
-                GUILayout.Label("Sorting: ");
-                int prevSortIndex = _sortWayIndex;
-                _sortWayIndex = EditorGUILayout.Popup(_sortWayIndex, _sortWays, GUILayout.Width(80f));
-                if (_sortWayIndex != prevSortIndex)
-                    Sort();
+                if (_result.HasAnyData())
+                {
+                    int prevSortIndex = _sortWayIndex;
+                    _sortWayIndex = GUILayout.Toolbar(_sortWayIndex, _sortWays);
+                    if (_sortWayIndex != prevSortIndex)
+                        Sort();
+                }
 
                 GUILayout.FlexibleSpace();
 
@@ -65,16 +73,17 @@ namespace UnityUtilityEditor.Window
             {
                 for (int i = 0; i < _result.Length; i++)
                 {
+                    ref Container container = ref _result[i];
+
                     EditorGUILayout.BeginHorizontal();
-
-                    GUILayout.Label(SizeToLable(_result[i].size, _sizeToolbarNames), GUILayout.Width(100f));
-                    GUILayout.Label(_result[i].folder);
-                    GUI.enabled = false;
-                    EditorGUILayout.ObjectField(_result[i].asset, typeof(UnityObject), false);
-                    GUI.enabled = true;
+                    bool clicked = GUILayout.Button(container.Asset.GetAssetIcon(), EditorStyles.label, _iconOptions);
+                    clicked |= GUILayout.Button(container.Path, EditorStyles.label);
+                    GUILayout.Label(SizeToLable(container.Size, _sizeToolbarNames));
                     GUILayout.FlexibleSpace();
-
                     EditorGUILayout.EndHorizontal();
+
+                    if (clicked)
+                        EditorGUIUtility.PingObject(container.Asset);
                 }
 
                 if (_result.Length == 0)
@@ -98,7 +107,7 @@ namespace UnityUtilityEditor.Window
                                       .Select(selector)
                                       .Where(item => item.size >= minSizeInBytes)
                                       .AsSequential()
-                                      .Select(createTuple)
+                                      .Select(item => new Container(item.path, item.size))
                                       .ToArray();
             Sort();
 
@@ -108,13 +117,6 @@ namespace UnityUtilityEditor.Window
                 string assetPath = filePath.Remove(0, projectFolderPath.Length + 1);
                 return (assetPath, info.Length);
             }
-
-            (UnityObject, string, long) createTuple((string path, long size) item)
-            {
-                const string slash = " ∕ ";
-                string prettyPath = PathUtility.GetParentPath(item.path).Replace("/", slash).Replace("\\", slash) + " ∕";
-                return (AssetDatabase.LoadAssetAtPath<UnityObject>(item.path), prettyPath, item.size);
-            }
         }
 
         private void Sort()
@@ -122,15 +124,15 @@ namespace UnityUtilityEditor.Window
             switch (_sortWayIndex)
             {
                 case 0:
-                    _result.Sort(item => item.asset.GetAssetPath());
+                    _result.Sort(item => item.Path);
                     break;
 
                 case 1:
-                    _result.Sort(item => item.size);
+                    _result.Sort(item => item.Size);
                     break;
 
                 case 2:
-                    _result.Sort(item => -item.size);
+                    _result.Sort(item => -item.Size);
                     break;
 
                 default:
@@ -141,15 +143,17 @@ namespace UnityUtilityEditor.Window
         private string SizeToLable(long size, string[] sizeToolbarNames)
         {
             if (size <= 1024)
-                return $"{size} {sizeToolbarNames[0]}";
+                return convert(size, sizeToolbarNames[0]);
 
             if (size <= 1024L * 1024L)
-                return $"{size / 1024d:F} {sizeToolbarNames[1]}";
+                return convert(size / 1024d, sizeToolbarNames[1]);
 
             if (size <= 1024L * 1024L * 1024L)
-                return $"{size / 1024d / 1024d:F} {sizeToolbarNames[2]}";
+                return convert(size / 1024d / 1024d, sizeToolbarNames[2]);
 
-            return $"{size / 1024d / 1024d / 1024d:F} {sizeToolbarNames[3]}";
+            return convert(size / 1024d / 1024d / 1024d, sizeToolbarNames[3]);
+
+            string convert(double value, string unit) => $"({value:F} {unit})";
         }
 
         private static long SizeField(long size, int sizeToolbarIndex)
@@ -192,6 +196,20 @@ namespace UnityUtilityEditor.Window
 
             return value;
 #endif
+        }
+
+        private struct Container
+        {
+            public string Path;
+            public UnityObject Asset;
+            public long Size;
+
+            public Container(string path, long size)
+            {
+                Size = size;
+                Asset = AssetDatabase.LoadAssetAtPath<UnityObject>(path);
+                Path = EditorGuiUtility.NicifyPathLabel(path);
+            }
         }
     }
 }
