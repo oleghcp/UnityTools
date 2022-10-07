1446c94e5c8aee0ce57286248c4550e8b8b3742b
Oleg Pulkin <oleghcp@gmail.com>
2022-10-07 13:51:54 +0300

    Xorshift64Rng

---
+++ Code/Runtime/UnityUtility/Rng/BytesBased/Xorshift64Bytes.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace UnityUtility.Rng.BytesBased
+{
+    [Serializable]
+    internal class Xorshift64Bytes : IRandomBytesProvider
+    {
+        private readonly int _a;
+        private readonly int _b;
+        private readonly int _c;
+
+        private byte[] _bytes = new byte[sizeof(ulong)];
+        private ulong _num64;
+        private int _counter;
+
+        public Xorshift64Bytes() : this(Environment.TickCount)
+        {
+
+        }
+
+        public Xorshift64Bytes(int seed) : this(seed, 13, 7, 17)
+        {
+
+        }
+
+        public Xorshift64Bytes(int seed, int a, int b, int c)
+        {
+            if (seed == 0)
+                throw new ArgumentException($"Parameter cannot be equal zero.", nameof(seed));
+
+            _num64 = (ulong)seed;
+
+            _a = a;
+            _b = b;
+            _c = c;
+        }
+
+        public void GetBytes(byte[] buffer)
+        {
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                if (_counter >= _bytes.Length)
+                    UpdateBytes();
+
+                buffer[i] = _bytes[_counter++];
+            }
+        }
+
+        public void GetBytes(Span<byte> buffer)
+        {
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                if (_counter >= _bytes.Length)
+                    UpdateBytes();
+
+                buffer[i] = _bytes[_counter++];
+            }
+        }
+
+        private unsafe void UpdateBytes()
+        {
+            ulong rn = Xorshift64();
+            byte* ptr = (byte*)&rn;
+            for (int j = 0; j < sizeof(ulong); j++)
+            {
+                _bytes[j] = ptr[j];
+            }
+            _counter = 0;
+        }
+
+        private ulong Xorshift64()
+        {
+            ulong x = _num64;
+            x ^= x << _a;
+            x ^= x >> _b;
+            x ^= x << _c;
+            return _num64 = x;
+        }
+    }
+}
---
+++ Code/Runtime/UnityUtility/Rng/Xorshift64Rng.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityUtility.Rng.BytesBased;
+
+namespace UnityUtility.Rng
+{
+    [Serializable]
+    public class Xorshift64Rng : BytesBasedRng
+    {
+        public Xorshift64Rng() : base(new Xorshift64Bytes())
+        {
+        }
+
+        public Xorshift64Rng(int seed) : base(new Xorshift64Bytes(seed))
+        {
+        }
+
+        public Xorshift64Rng(int seed, int a, int b, int c) : base(new Xorshift64Bytes(seed, a, b, c))
+        {
+        }
+    }
+}
