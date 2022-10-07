f43aa6416ee723cc1874d4efa6054775f10619dc
Oleg Pulkin <oleghcp@gmail.com>
2022-10-07 15:43:26 +0300

    upd BytesBasedRng

--- Code/Runtime/UnityUtility/Rng/BytesBased/BytesBasedRng.cs
+++ Code/Runtime/UnityUtility/Rng/BytesBased/BytesBasedRng.cs
@@ -5,11 +5,10 @@ namespace UnityUtility.Rng.BytesBased
     public class BytesBasedRng : RandomNumberGenerator
     {
         private IRandomBytesProvider _rbp;
-#if !UNITY_2021_2_OR_NEWER
-        private byte[] _bytes32 = new byte[sizeof(uint)];
-        private byte[] _bytes16 = new byte[sizeof(ushort)];
+
         private byte[] _bytes8 = new byte[sizeof(byte)];
-#endif
+        private byte[] _bytes16 = new byte[sizeof(ushort)];
+        private byte[] _bytes32 = new byte[sizeof(uint)];
 
         public BytesBasedRng(IRandomBytesProvider randomBytesProvider)
         {
@@ -52,38 +51,20 @@ namespace UnityUtility.Rng.BytesBased
 
         private byte RandomUint8()
         {
-#if UNITY_2021_2_OR_NEWER
-            Span<byte> bytes = stackalloc byte[sizeof(byte)];
-            _rbp.GetBytes(bytes);
-            return bytes[0];
-#else
             _rbp.GetBytes(_bytes8);
             return _bytes8[0];
-#endif
         }
 
         private ushort RandomUint16()
         {
-#if UNITY_2021_2_OR_NEWER
-            Span<byte> bytes = stackalloc byte[sizeof(ushort)];
-            _rbp.GetBytes(bytes);
-            return BitConverter.ToUInt16(bytes);
-#else
             _rbp.GetBytes(_bytes16);
             return BitConverter.ToUInt16(_bytes16, 0);
-#endif
         }
 
         private uint RandomUint32()
         {
-#if UNITY_2021_2_OR_NEWER
-            Span<byte> bytes = stackalloc byte[sizeof(uint)];
-            _rbp.GetBytes(bytes);
-            return BitConverter.ToUInt32(bytes);
-#else
             _rbp.GetBytes(_bytes32);
             return BitConverter.ToUInt32(_bytes32, 0);
-#endif
         }
     }
 }
