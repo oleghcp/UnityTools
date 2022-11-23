67c1306e62e8d481f31ea20ffa611047c05a7ef1
Oleg Pulkin <oleghcp@gmail.com>
2022-11-23 20:05:14 +0300

    RoutineExtensions

---
+++ Code/Runtime/UnityUtility/Async/RoutineExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Threading;
+
+namespace UnityUtility.Async
+{
+    public static class RoutineExtensions
+    {
+        public static TaskInfo StartAsync(this IEnumerator self)
+        {
+            return TaskSystem.StartAsync(self, default);
+        }
+
+        public static TaskInfo StartAsync(this IEnumerator self, in CancellationToken token)
+        {
+            return TaskSystem.StartAsync(self, token);
+        }
+
+        public static TaskInfo StartAsyncLocally(this IEnumerator self)
+        {
+            return TaskSystem.StartAsyncLocally(self, default);
+        }
+
+        public static TaskInfo StartAsyncLocally(this IEnumerator self, in CancellationToken token)
+        {
+            return TaskSystem.StartAsyncLocally(self, token);
+        }
+    }
+}
