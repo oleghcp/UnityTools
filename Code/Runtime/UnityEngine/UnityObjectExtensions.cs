1eddb2c467ee2b6907709f72d9ba5d838e5ca7ba
Oleg Pulkin <oleghcp@gmail.com>
2022-11-26 19:20:38 +0300

    fix UnityObjectExtensions

--- Code/Runtime/UnityEngine/UnityObjectExtensions.cs
+++ Code/Runtime/UnityEngine/UnityObjectExtensions.cs
@@ -83,25 +83,6 @@ namespace UnityEngine
             return filter;
         }
 
-        public static Vector2 GetOrthographicSize(this Camera self)
-        {
-            return ScreenUtility.GetOrthographicSize(self.orthographicSize);
-        }
-
-        public static Vector2 GetPerspectiveSize(this Camera self, float remoteness)
-        {
-            return ScreenUtility.GetPerspectiveSize(self.fieldOfView, remoteness);
-        }
-
-        public static float GetHorizontalFov(this Camera self)
-        {
-#if UNITY_2019_1_OR_NEWER
-            return Camera.VerticalToHorizontalFieldOfView(self.fieldOfView, (float)Screen.width / Screen.height);
-#else
-            return ScreenUtility.GetAspectAngle(self.fieldOfView, (float)Screen.width / Screen.height);
-#endif
-        }
-
         /// <summary>
         /// Creates a sprite.
         /// </summary>
