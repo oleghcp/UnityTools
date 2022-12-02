319fc3c4196fd3da667bc305f0581512dddc2267
Oleg Pulkin <oleghcp@gmail.com>
2022-12-02 19:40:00 +0300

    AssetRef

---
+++ Code/Editor/Drawers/AssetRefDrawer.cs
@@ -0,0 +1,52 @@
+﻿#if UNITY_2020_1_OR_NEWER
+using System;
+using UnityEditor;
+using UnityEngine;
+using UnityUtility;
+using UnityUtilityEditor.Engine;
+using UnityObject = UnityEngine.Object;
+
+namespace UnityUtilityEditor.Drawers
+{
+    [CustomPropertyDrawer(typeof(AssetRef<>))]
+    public class AssetRefDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            SerializedProperty flagProp = property.FindPropertyRelative(AssetRef<UnityObject>.FlagFieldName);
+            SerializedProperty dirRefProp = property.FindPropertyRelative(AssetRef<UnityObject>.DirectRefFieldName);
+            SerializedProperty lazyRefProp = property.FindPropertyRelative(AssetRef<UnityObject>.LazyRefFieldName);
+
+            const float buttonText = 40f;
+            Type fieldType = EditorUtilityExt.GetFieldType(this);
+
+            position = EditorGUI.PrefixLabel(position, label);
+
+            Rect rect = position;
+            rect.width = buttonText;
+            bool prevFlagValue = flagProp.boolValue;
+            flagProp.boolValue = EditorGui.ToggleButton(rect, "Lazy", prevFlagValue);
+
+            bool lazy = flagProp.boolValue;
+            if (prevFlagValue != lazy)
+            {
+                SerializedProperty source = lazy ? dirRefProp : lazyRefProp;
+                SerializedProperty dest = lazy ? lazyRefProp : dirRefProp;
+
+                dest.objectReferenceValue = source.objectReferenceValue;
+                source.objectReferenceValue = null;
+            }
+
+            rect = position;
+            rect.xMin += buttonText + EditorGuiUtility.StandardHorizontalSpacing;
+            SerializedProperty drawnProp = lazy ? lazyRefProp : dirRefProp;
+            drawnProp.objectReferenceValue = EditorGUI.ObjectField(rect, drawnProp.objectReferenceValue, fieldType.GenericTypeArguments[0], false);
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+    }
+}
+#endif
---
+++ Code/Runtime/UnityUtility/AssetRef.cs
@@ -0,0 +1,30 @@
+﻿#if UNITY_2020_1_OR_NEWER
+using System;
+using UnityEngine;
+using UnityObject = UnityEngine.Object;
+
+namespace UnityUtility
+{
+    [Serializable]
+    public struct AssetRef<T> where T : UnityObject
+    {
+        [SerializeField]
+        private T _directRef;
+        [SerializeField]
+        private LazyLoadReference<T> _lazyRef;
+        [SerializeField]
+        private bool _lazy;
+
+#if UNITY_EDITOR
+        public static string DirectRefFieldName => nameof(_directRef);
+        public static string LazyRefFieldName => nameof(_lazyRef);
+        public static string FlagFieldName => nameof(_lazy);
+#endif
+
+        public int InstanceId => _lazy ? _lazyRef.instanceID : _directRef.GetInstanceID();
+        public bool Initialized => _lazy ? _lazyRef.isSet : _directRef != null;
+        public bool Lazy => _lazy;
+        public T Asset => _lazy ? _lazyRef.asset : _directRef;
+    }
+}
+#endif
