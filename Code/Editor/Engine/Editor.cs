63d3c2a6af86f262e4a94c8c2d6f9f88f22a662a
Oleg Pulkin <oleghcp@gmail.com>
2022-12-03 20:06:07 +0300

    upd Editor.cs

--- Code/Editor/Engine/Editor.cs
+++ Code/Editor/Engine/Editor.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityObject = UnityEngine.Object;
 
 namespace UnityUtilityEditor.Engine
 {
-    public abstract class Editor<T> : Editor where T : UnityObject
+    public abstract class Editor<T> : Editor, IReadOnlyList<T> where T : UnityObject
     {
 #pragma warning disable IDE1006
         public new T target
@@ -12,16 +13,26 @@ namespace UnityUtilityEditor.Engine
             get => base.target as T;
             set => base.target = value;
         }
+
+        public new IReadOnlyList<T> targets => this;
 #pragma warning restore IDE1006
 
-        public IEnumerable<T> EnumerateTargets()
-        {
-            UnityObject[] targetsArray = targets;
+        T IReadOnlyList<T>.this[int index] => (T)base.targets[index];
+        int IReadOnlyCollection<T>.Count => base.targets.Length;
 
-            for (int i = 0; i < targetsArray.Length; i++)
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            UnityObject[] array = base.targets;
+
+            for (int i = 0; i < array.Length; i++)
             {
-                yield return targetsArray[i] as T;
+                yield return (T)array[i];
             }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return base.targets.GetEnumerator();
         }
     }
 }
