8c38a0486318c4edabf50f5b0ea143e473f6d0e8
Oleg Pulkin <oleghcp@gmail.com>
2023-01-04 18:27:05 +0300

    refactoring of diapason drawers

--- Code/Editor/Drawers/DiapasonDrawer.cs
+++ Code/Editor/Drawers/DiapasonDrawer.cs
@@ -2,45 +2,48 @@
 using UnityEngine;
 using UnityUtility.Mathematics;
 using UnityUtility.NumericEntities;
-using UnityUtilityEditor.Engine;
 
 namespace UnityUtilityEditor.Drawers
 {
     [CustomPropertyDrawer(typeof(Diapason))]
-    [CustomPropertyDrawer(typeof(DiapasonInt))]
     internal class DiapasonDrawer : PropertyDrawer
     {
-        private GUIContent[] _floatSubLabels = { new GUIContent(Diapason.MinFieldName), new GUIContent(Diapason.MaxFieldName) };
-        private GUIContent[] _intSubLabels = { new GUIContent(DiapasonInt.FromFieldName), new GUIContent(DiapasonInt.BeforeFieldName) };
+        private GUIContent[] _subLabels = { new GUIContent(Diapason.MinFieldName), new GUIContent(Diapason.MaxFieldName) };
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             position = EditorGUI.PrefixLabel(position, label);
 
-            if (EditorUtilityExt.GetFieldType(this) == typeof(Diapason))
-            {
-                SerializedProperty min = property.FindPropertyRelative(_floatSubLabels[0].text);
-                SerializedProperty max = property.FindPropertyRelative(_floatSubLabels[1].text);
+            SerializedProperty min = property.FindPropertyRelative(_subLabels[0].text);
+            SerializedProperty max = property.FindPropertyRelative(_subLabels[1].text);
 
-                float[] array = { min.floatValue, max.floatValue };
+            float[] array = { min.floatValue, max.floatValue };
 
-                EditorGUI.MultiFloatField(position, _floatSubLabels, array);
+            EditorGUI.MultiFloatField(position, _subLabels, array);
 
-                min.floatValue = array[0];
-                max.floatValue = array[1].ClampMin(array[0]);
-            }
-            else
-            {
-                SerializedProperty min = property.FindPropertyRelative(_intSubLabels[0].text);
-                SerializedProperty max = property.FindPropertyRelative(_intSubLabels[1].text);
+            min.floatValue = array[0];
+            max.floatValue = array[1].ClampMin(array[0]);
+        }
+    }
 
-                int[] array = { min.intValue, max.intValue };
+    [CustomPropertyDrawer(typeof(DiapasonInt))]
+    internal class DiapasonIntDrawer : PropertyDrawer
+    {
+        private GUIContent[] _subLabels = { new GUIContent(DiapasonInt.FromFieldName), new GUIContent(DiapasonInt.BeforeFieldName) };
 
-                EditorGUI.MultiIntField(position, _intSubLabels, array);
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            position = EditorGUI.PrefixLabel(position, label);
 
-                min.intValue = array[0];
-                max.intValue = array[1].ClampMin(array[0] + 1);
-            }
+            SerializedProperty min = property.FindPropertyRelative(_subLabels[0].text);
+            SerializedProperty max = property.FindPropertyRelative(_subLabels[1].text);
+
+            int[] array = { min.intValue, max.intValue };
+
+            EditorGUI.MultiIntField(position, _subLabels, array);
+
+            min.intValue = array[0];
+            max.intValue = array[1].ClampMin(array[0] + 1);
         }
     }
 }
