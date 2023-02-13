788fea2fdd5cfea0a9d3853d4fe9419cd5ac3ecb
Oleg Pulkin <oleghcp@gmail.com>
2023-02-13 20:22:50 +0300

    upd SingleBehaviour with FindObjectOfType<T>(true);

--- Code/Runtime/UnityUtility/SingleScripts/SingleBehaviour.cs
+++ Code/Runtime/UnityUtility/SingleScripts/SingleBehaviour.cs
@@ -23,7 +23,11 @@ namespace UnityUtility.SingleScripts
             {
                 if (_instance == null)
                 {
+#if UNITY_2020_1_OR_NEWER
+                    T instance = FindObjectOfType<T>(true);
+#else
                     T instance = FindObjectOfType<T>();
+#endif
 
                     if (instance == null)
                         throw new ObjectNotFoundException($"There is no any instance of {typeof(T).Name}.");
