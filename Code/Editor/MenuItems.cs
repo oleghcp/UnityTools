51e324502eceb14d1e57ff98c8271ff1dafdb14c
Oleg Pulkin <oleghcp@gmail.com>
2023-03-07 19:24:51 +0300

    change menu items path

--- Code/Editor/MenuItems.cs
+++ Code/Editor/MenuItems.cs
@@ -12,40 +12,41 @@ namespace UnityUtilityEditor
 {
     internal static class MenuItems
     {
+        public const string MAIN_MENU_NAME = "Tools/" + LibConstants.LIB_NAME + "/";
         public const string CONTEXT_MENU_NAME = "CONTEXT/";
         public const string RESET_ITEM_NAME = "Reset";
 
-        [MenuItem(LibConstants.LIB_NAME + "/About", false, 1)]
+        [MenuItem(MAIN_MENU_NAME + "About", false, 1)]
         private static void GetAboutWindow()
         {
             EditorWindow.GetWindow(typeof(AboutWindow), true, "About");
         }
 
-        [MenuItem(LibConstants.LIB_NAME + "/Objects/Meshes/Create Rect Plane")]
+        [MenuItem(MAIN_MENU_NAME + "Objects/Meshes/Create Rect Plane")]
         private static void GetCreateRectPlaneWizard()
         {
             ScriptableWizard.DisplayWizard("Create Rect Plane", typeof(CreateRectPlaneWizard));
         }
 
-        [MenuItem(LibConstants.LIB_NAME + "/Objects/Meshes/Create Figured Plane")]
+        [MenuItem(MAIN_MENU_NAME + "Objects/Meshes/Create Figured Plane")]
         private static void GetCreateFiguredPlaneWizard()
         {
             ScriptableWizard.DisplayWizard("Create Figured Plane", typeof(CreateFiguredPlaneWizard));
         }
 
-        [MenuItem(LibConstants.LIB_NAME + "/Objects/Meshes/Create Shape")]
+        [MenuItem(MAIN_MENU_NAME + "Objects/Meshes/Create Shape")]
         private static void GetCreateShapeWizard()
         {
             ScriptableWizard.DisplayWizard("Create Shape", typeof(CreateShapeWizard));
         }
 
-        [MenuItem(LibConstants.LIB_NAME + "/Objects/Create Scriptable Object Asset")]
+        [MenuItem(MAIN_MENU_NAME + "Objects/Create Scriptable Object Asset")]
         private static void GetScriptableObjectWindow()
         {
             EditorWindow.GetWindow(typeof(CreateAssetWindow), true, "Create Asset");
         }
 
-        [MenuItem(LibConstants.LIB_NAME + "/Terminal/Create Terminal Prefab")]
+        [MenuItem(MAIN_MENU_NAME + "Terminal/Create Terminal Prefab")]
         private static void CreateTerminalPrefab()
         {
             string assetPath = AssetDatabase.GUIDToAssetPath("7537be9dac6f69045a2656580dc951f0");
@@ -59,62 +60,62 @@ namespace UnityUtilityEditor
             AssetDatabase.SaveAssets();
         }
 
-        [MenuItem(LibConstants.LIB_NAME + "/Code/Generate Layer Set Class")]
+        [MenuItem(MAIN_MENU_NAME + "Code/Generate Layer Set Class")]
         private static void GenerateLayerSetClass()
         {
             LayerSetWindow.CreateWindow();
         }
 
-        [MenuItem(LibConstants.LIB_NAME + "/Folders/Open Project Folder")]
+        [MenuItem(MAIN_MENU_NAME + "Folders/Open Project Folder")]
         private static void OpenProjectFolder()
         {
             EditorUtilityExt.OpenFolder(PathUtility.GetParentPath(Application.dataPath));
         }
 
-        [MenuItem(LibConstants.LIB_NAME + "/Folders/Open Persistent Data Folder")]
+        [MenuItem(MAIN_MENU_NAME + "Folders/Open Persistent Data Folder")]
         private static void OpenPersistentDataFolder()
         {
             EditorUtilityExt.OpenFolder(Application.persistentDataPath);
         }
 
-        [MenuItem(LibConstants.LIB_NAME + "/Folders/Remove Empty Folders")]
+        [MenuItem(MAIN_MENU_NAME + "Folders/Remove Empty Folders")]
         private static void RemoveEmptyFolders()
         {
             MenuItemsUtility.RemoveEmptyFolders();
         }
 
-        [MenuItem(LibConstants.LIB_NAME + "/Files/Convert Code Files to UTF8")]
+        [MenuItem(MAIN_MENU_NAME + "Files/Convert Code Files to UTF8")]
         private static void ConvertCodeFilesToUtf8()
         {
             AssetDatabaseExt.ConvertCodeFilesToUtf8();
         }
 
-        [MenuItem(LibConstants.LIB_NAME + "/Files/Convert Text Files to UTF8")]
+        [MenuItem(MAIN_MENU_NAME + "Files/Convert Text Files to UTF8")]
         private static void ConvertTextFilesToUtf8()
         {
             AssetDatabaseExt.ConvertTextFilesToUtf8();
         }
 
-        [MenuItem(LibConstants.LIB_NAME + "/Files/Find Huge Files")]
+        [MenuItem(MAIN_MENU_NAME + "Files/Find Huge Files")]
         private static void FindHugeFiles()
         {
             SearchHugeFilesWindow.Create();
         }
 
 #if UNITY_2019_3_OR_NEWER && INCLUDE_ADDRESSABLES && INCLUDE_NEWTONSOFT_JSON
-        [MenuItem(LibConstants.LIB_NAME + "/Addressables/Analysis Results")]
+        [MenuItem(MAIN_MENU_NAME + "Addressables/Analysis Results")]
         private static void OpenAddressablesAnalysisResultsWindow()
         {
             EditorWindow.GetWindow<AddressablesAnalysisResultsWindow>(false, "Analysis Results", true);
         }
 #endif
-        [MenuItem(LibConstants.LIB_NAME + "/Capture Screen/Take Screenshot")]
+        [MenuItem(MAIN_MENU_NAME + "Capture Screen/Take Screenshot")]
         private static void Screenshot()
         {
             ScreenCapture.CaptureScreenshot($"Screenshot_{Helper.GetDateTimeString()}.png");
         }
 
-        [MenuItem(LibConstants.LIB_NAME + "/Capture Screen/Take Screenshot as")]
+        [MenuItem(MAIN_MENU_NAME + "Capture Screen/Take Screenshot as")]
         private static void ScreenshotAs()
         {
             string path = EditorUtility.SaveFilePanel("Take Screenshot", Application.dataPath, $"Screenshot_{Helper.GetDateTimeString()}", "png");
