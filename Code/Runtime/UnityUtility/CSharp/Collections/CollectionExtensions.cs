d9d8f4c90efefc861315f298a102e3da2d10fc02
Oleg Pulkin <oleghcp@gmail.com>
2023-04-06 21:46:14 +0300

    remove unused extensions

--- Code/Runtime/UnityUtility/CSharp/Collections/CollectionExtensions.cs
+++ Code/Runtime/UnityUtility/CSharp/Collections/CollectionExtensions.cs
@@ -494,24 +494,6 @@ namespace UnityUtility.CSharp.Collections
         }
 
         /// <summary>
-        /// Inserts an element into the list at the specified index. Expands the list if the index is out of bounds.
-        /// </summary>
-        public static void ForceInsert<T>(this IList<T> self, int index, T newItem)
-        {
-            while (index > self.Count) { self.Add(default); }
-            self.Insert(index, newItem);
-        }
-
-        /// <summary>
-        /// Inserts an element into the list at the specified index and returns that element. Expands the list if the index is out of bounds.
-        /// </summary>
-        public static T ForcePush<T>(this IList<T> self, int index, T newItem)
-        {
-            self.ForceInsert(index, newItem);
-            return newItem;
-        }
-
-        /// <summary>
         /// Adds an element to the dictionary and returns that element.
         /// </summary>
         public static TValue Place<TKey, TValue>(this IDictionary<TKey, TValue> self, TKey key, TValue newItem)
