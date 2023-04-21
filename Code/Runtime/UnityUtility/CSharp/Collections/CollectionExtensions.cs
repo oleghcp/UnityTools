da66bf84bc634d33c369530e959bc2735bc6a304
Oleg Pulkin <oleghcp@gmail.com>
2023-04-21 21:40:16 +0300

    EnumerableExtensions

--- Code/Runtime/UnityUtility/CSharp/Collections/CollectionExtensions.cs
+++ Code/Runtime/UnityUtility/CSharp/Collections/CollectionExtensions.cs
@@ -182,42 +182,6 @@ namespace UnityUtility.CSharp.Collections
         }
 
         /// <summary>
-        /// Returns an element with the minimum parameter value.
-        /// </summary>
-        public static TSource GetWithMin<TSource, TKey>(this IEnumerable<TSource> self, Func<TSource, TKey> selector)
-        {
-            CollectionUtility.Min(self, selector, out TSource res, out _);
-            return res;
-        }
-
-        /// <summary>
-        /// Returns an element with the maximum parameter value.
-        /// </summary>
-        public static TSource GetWithMax<TSource, TKey>(this IEnumerable<TSource> self, Func<TSource, TKey> selector)
-        {
-            CollectionUtility.Max(self, selector, out TSource res, out _);
-            return res;
-        }
-
-        /// <summary>
-        /// Returns an element with the minimum parameter value.
-        /// </summary>
-        public static TSource GetWithMin<TSource, TKey>(this IEnumerable<TSource> self, Func<TSource, TKey> selector, out TKey min)
-        {
-            CollectionUtility.Min(self, selector, out TSource res, out min);
-            return res;
-        }
-
-        /// <summary>
-        /// Returns an element with the maximum parameter value.
-        /// </summary>
-        public static TSource GetWithMax<TSource, TKey>(this IEnumerable<TSource> self, Func<TSource, TKey> selector, out TKey max)
-        {
-            CollectionUtility.Max(self, selector, out TSource res, out max);
-            return res;
-        }
-
-        /// <summary>
         /// Indicates whether the specified collection is null or it's length equals zero.
         /// </summary>        
         public static bool IsNullOrEmpty<T>(this ICollection<T> self)
@@ -232,38 +196,6 @@ namespace UnityUtility.CSharp.Collections
         {
             return self != null && self.Count > 0;
         }
-
-        /// <summary>
-        /// Indicates whether the specified collection is null or it's length equals zero.
-        /// </summary>    
-        public static bool IsNullOrEmpty<T>(this IEnumerable<T> self)
-        {
-            if (self == null)
-                return true;
-
-            if (self is ICollection<T> collection)
-                return collection.Count == 0;
-
-            if (self is IReadOnlyCollection<T> readOnlyColl)
-                return readOnlyColl.Count == 0;
-
-            return !self.GetEnumerator().MoveNext();
-        }
-
-        /// <summary>
-        /// Indicates whether the specified collection is not null and contains at least one element.
-        /// </summary>
-        public static bool HasAnyData<T>(this IEnumerable<T> self)
-        {
-            return !self.IsNullOrEmpty();
-        }
-
-#if !UNITY_2021_2_OR_NEWER
-        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> self)
-        {
-            return new HashSet<T>(self);
-        }
-#endif
 
         /// <summary>
         /// Returns the element at the specified index from the end of a collection.
@@ -456,15 +388,6 @@ namespace UnityUtility.CSharp.Collections
         {
             for (int i = 0; i < self.Count; i++)
                 action(self[i]);
-        }
-
-        /// <summary>
-        /// Performs the specified action on each element of the System.Collections.Generic.IEnumerable`1.
-        /// </summary>
-        public static void ForEach<T>(this IEnumerable<T> self, Action<T> action)
-        {
-            foreach (var item in self)
-                action(item);
         }
 
         /// <summary>
