95e9fb9eed26c386b931ee777bd7d4b603623e34
Oleg Pulkin <oleghcp@gmail.com>
2024-02-28 00:35:26 +0300

    upd BoolInt

--- Code/Runtime/OlegHcp/BoolInt.cs
+++ Code/Runtime/OlegHcp/BoolInt.cs
@@ -1,19 +1,38 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
-using UnityEngine;
 using OlegHcp.Mathematics;
+using OlegHcp.Tools;
+using UnityEngine;
 
 namespace OlegHcp
 {
     [Serializable]
-    public struct BoolInt : IEquatable<BoolInt>, IComparable<BoolInt>
+    public struct BoolInt : IEquatable<BoolInt>
     {
         [SerializeField, HideInInspector]
         private int _value;
+        [SerializeField, HideInInspector]
+        private bool _throwIfNegative;
+
+        public bool ThrowIfNegative => _throwIfNegative;
+        public int Count => _value;
 
         public BoolInt(int defaultValue)
         {
             _value = defaultValue;
+            _throwIfNegative = false;
+        }
+
+        public BoolInt(bool throwIfNegative)
+        {
+            _value = 0;
+            _throwIfNegative = throwIfNegative;
+        }
+
+        public BoolInt(int defaultValue, bool throwIfNegative)
+        {
+            _value = defaultValue;
+            _throwIfNegative = throwIfNegative;
         }
 
         public void AddValue(bool value)
@@ -39,6 +58,11 @@ namespace OlegHcp
             _value = 0;
         }
 
+        public bool ToBool()
+        {
+            return this;
+        }
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private void Increment()
         {
@@ -48,12 +72,15 @@ namespace OlegHcp
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private void Decrement()
         {
+            if (_throwIfNegative && _value == 0)
+                throw new InvalidOperationException("Does not support negative values.");
+
             checked { _value--; }
         }
 
         public override int GetHashCode()
         {
-            return _value.GetHashCode();
+            return Helper.GetHashCode(_value, _throwIfNegative.GetHashCode());
         }
 
         public override string ToString()
@@ -68,17 +95,12 @@ namespace OlegHcp
 
         public bool Equals(BoolInt other)
         {
-            return other._value == _value;
-        }
-
-        public int CompareTo(BoolInt other)
-        {
-            return _value.CompareTo(other._value);
+            return other._value == _value && other._throwIfNegative == _throwIfNegative;
         }
 
         public static implicit operator bool(BoolInt value)
         {
-            return value._value.ToBool();
+            return value._value > 0;
         }
 
         public static explicit operator int(BoolInt value)
@@ -86,14 +108,16 @@ namespace OlegHcp
             return value._value;
         }
 
-        public static bool operator ==(BoolInt a, BoolInt b)
+        public static BoolInt operator ++(BoolInt value)
         {
-            return a.Equals(b);
+            value.Increment();
+            return value;
         }
 
-        public static bool operator !=(BoolInt a, BoolInt b)
+        public static BoolInt operator --(BoolInt value)
         {
-            return !a.Equals(b);
+            value.Decrement();
+            return value;
         }
     }
 }
