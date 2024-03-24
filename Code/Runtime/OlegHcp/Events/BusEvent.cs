a0ce1e9d8a77730227443f9eadb60e5a15f48ad9
Oleg Pulkin <oleghcp@gmail.com>
2024-03-24 22:22:30 +0300

    SignalBus

---
+++ Code/Runtime/OlegHcp/Events/BusEvent.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OlegHcp.CSharp.Collections;
+
+namespace OlegHcp.Events
+{
+    public interface IEvent
+    {
+        void Invoke<T>(T signal) where T : ISignal;
+        void UnregisterOwner();
+    }
+
+    internal class BusEvent : IEvent
+    {
+        private HashSet<EventSubscription> _callbacks = new HashSet<EventSubscription>();
+        private object _owner;
+
+        public Type SignalType { get; }
+
+        public BusEvent(Type signalType)
+        {
+            SignalType = signalType;
+        }
+
+        public bool TrySetOwner(object owner)
+        {
+            if (_owner != null)
+                return false;
+
+            _owner = owner;
+            return true;
+        }
+
+        public void UnregisterOwner()
+        {
+            _owner = null;
+        }
+
+        public void Add<T>(Action<T> callback, int priority) where T : ISignal
+        {
+            _callbacks.Add(new EventSubscription(callback, priority));
+        }
+
+        public void Remove<T>(Action<T> callback) where T : ISignal
+        {
+            _callbacks.Remove(new EventSubscription(callback));
+        }
+
+        public void Invoke<T>(T signal) where T : ISignal
+        {
+            _callbacks.OrderBy(item => item.Priority)
+                      .ForEach(item => item.Invoke(signal));
+        }
+    }
+}
---
+++ Code/Runtime/OlegHcp/Events/SignalBus.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using OlegHcp.CSharp.Collections;
+
+namespace OlegHcp.Events
+{
+    public interface ISignal
+    {
+
+    }
+
+    public class SignalBus
+    {
+        private Dictionary<Type, BusEvent> _storage = new Dictionary<Type, BusEvent>();
+
+        public void Subscribe<T>(Action<T> callback) where T : ISignal
+        {
+            Subscribe(int.MaxValue, callback);
+        }
+
+        public void Subscribe<T>(int priority, Action<T> callback) where T : ISignal
+        {
+            if (callback == null)
+                throw new ArgumentNullException();
+
+            GetEvent(typeof(T)).Add(callback, priority);
+        }
+
+        public void Unsubscribe<T>(Action<T> callback) where T : ISignal
+        {
+            if (callback == null)
+                throw new ArgumentNullException();
+
+            if (_storage.TryGetValue(typeof(T), out BusEvent @event))
+                @event.Remove(callback);
+        }
+
+        public IEvent RegisterEventOwner<T>(object owner) where T : ISignal
+        {
+            if (owner == null)
+                throw new ArgumentNullException();
+
+            BusEvent @event = GetEvent(typeof(T));
+
+            if (@event.TrySetOwner(owner))
+                return @event;
+
+            throw new Exception();
+        }
+
+        private BusEvent GetEvent(Type type)
+        {
+            if (_storage.TryGetValue(type, out BusEvent @event))
+                return @event;
+
+            return _storage.Place(type, new BusEvent(type));
+        }
+    }
+}
