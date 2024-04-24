b6d9303ad1867e2321bd45057b2bc06d1e29c51b
Oleg Pulkin <oleghcp@gmail.com>
2024-04-24 10:24:37 +0300

    upd ArrayUtility

--- Code/Runtime/OlegHcp/CSharp/ArrayUtility.cs
+++ Code/Runtime/OlegHcp/CSharp/ArrayUtility.cs
@@ -6,67 +6,67 @@ namespace OlegHcp.CSharp
     internal static class ArrayUtility
     {
         #region Sort
-        public static void Sort<T>(T[] collection, int left, int right) where T : IComparable<T>
+        public static void Sort<T>(T[] array, int left, int right) where T : IComparable<T>
         {
             if (left >= right)
                 return;
 
             int i = left, j = right;
-            T pivot = collection[left];
+            T pivot = array[left];
 
             while (i <= j)
             {
-                while (collection[i].CompareTo(pivot) < 0) { i++; }
-                while (collection[j].CompareTo(pivot) > 0) { j--; }
+                while (array[i].CompareTo(pivot) < 0) { i++; }
+                while (array[j].CompareTo(pivot) > 0) { j--; }
 
                 if (i <= j)
-                    collection.Swap(i++, j--);
+                    array.Swap(i++, j--);
             }
 
-            Sort(collection, left, j);
-            Sort(collection, i, right);
+            Sort(array, left, j);
+            Sort(array, i, right);
         }
 
-        public static void Sort<T, TComp>(T[] collection, int left, int right, TComp comparer) where TComp : IComparer<T>
+        public static void Sort<T, TComp>(T[] array, int left, int right, TComp comparer) where TComp : IComparer<T>
         {
             if (left >= right)
                 return;
 
             int i = left, j = right;
-            T pivot = collection[left];
+            T pivot = array[left];
 
             while (i <= j)
             {
-                while (comparer.Compare(collection[i], pivot) < 0) { i++; }
-                while (comparer.Compare(collection[j], pivot) > 0) { j--; }
+                while (comparer.Compare(array[i], pivot) < 0) { i++; }
+                while (comparer.Compare(array[j], pivot) > 0) { j--; }
 
                 if (i <= j)
-                    collection.Swap(i++, j--);
+                    array.Swap(i++, j--);
             }
 
-            Sort(collection, left, j, comparer);
-            Sort(collection, i, right, comparer);
+            Sort(array, left, j, comparer);
+            Sort(array, i, right, comparer);
         }
 
-        public static void Sort<T>(T[] collection, int left, int right, Comparison<T> comparison)
+        public static void Sort<T>(T[] array, int left, int right, Comparison<T> comparison)
         {
             if (left >= right)
                 return;
 
             int i = left, j = right;
-            T pivot = collection[left];
+            T pivot = array[left];
 
             while (i <= j)
             {
-                while (comparison(collection[i], pivot) < 0) { i++; }
-                while (comparison(collection[j], pivot) > 0) { j--; }
+                while (comparison(array[i], pivot) < 0) { i++; }
+                while (comparison(array[j], pivot) > 0) { j--; }
 
                 if (i <= j)
-                    collection.Swap(i++, j--);
+                    array.Swap(i++, j--);
             }
 
-            Sort(collection, left, j, comparison);
-            Sort(collection, i, right, comparison);
+            Sort(array, left, j, comparison);
+            Sort(array, i, right, comparison);
         }
         #endregion
     }
