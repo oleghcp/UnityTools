5aace2fcd1a5848b64992e807b4fadf323df9e3e
Oleg Pulkin <oleghcp@gmail.com>
2024-04-27 08:40:16 +0300

    SeparatorAttribute

---
+++ Code/Editor/Drawers/Attributes/SeparatorDrawer.cs
@@ -0,0 +1,31 @@
+﻿using OlegHcp.Inspector;
+using OlegHcpEditor.Engine;
+using UnityEditor;
+using UnityEngine;
+
+namespace OlegHcpEditor.Drawers.Attributes
+{
+    [CustomPropertyDrawer(typeof(SeparatorAttribute))]
+    internal class SeparatorDrawer : AttributeDrawer<SeparatorAttribute>
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            Rect rect = position;
+            rect.yMin += EditorGUIUtility.singleLineHeight * 0.25f;
+            rect.height = attribute.Height;
+            EditorGUI.DrawRect(rect, attribute.Color);
+
+            rect = position;
+            rect.yMin += attribute.Height + EditorGUIUtility.singleLineHeight * 0.5f + EditorGUIUtility.standardVerticalSpacing;
+            property.Draw(rect, label, true);
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return property.GetHeight(label, true) +
+                   attribute.Height +
+                   EditorGUIUtility.standardVerticalSpacing +
+                   EditorGUIUtility.singleLineHeight * 0.5f;
+        }
+    }
+}
---
+++ Code/Runtime/OlegHcp/Inspector/SeparatorAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using OlegHcp.Mathematics;
+using UnityEngine;
+
+namespace OlegHcp.Inspector
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class SeparatorAttribute : PropertyAttribute
+    {
+        const float DEFAULT_HEIGHT = 2f;
+        const ColorCode DEFAULT_COLOR = ColorCode.Grey;
+
+        internal Color Color { get; }
+        internal float Height { get; }
+
+        public SeparatorAttribute(float height, ColorCode color)
+        {
+            Color = color.ToColor();
+            Height = height.ClampMin(0f);
+        }
+
+        public SeparatorAttribute(float height) : this(height, DEFAULT_COLOR)
+        {
+
+        }
+
+        public SeparatorAttribute(ColorCode color) : this(DEFAULT_HEIGHT, color)
+        {
+
+        }
+
+        public SeparatorAttribute() : this(DEFAULT_HEIGHT, DEFAULT_COLOR)
+        {
+            
+        }
+    }
+}
