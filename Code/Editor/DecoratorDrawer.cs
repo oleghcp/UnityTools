ba28286d6568fe449da0608b73234bd3c4a78371
Oleg Pulkin <oleghcp@gmail.com>
2024-04-28 07:25:08 +0300

    parameterized DecoratorDrawer

---
+++ Code/Editor/DecoratorDrawer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace OlegHcpEditor
+{
+    public class DecoratorDrawer<TAttribute> : DecoratorDrawer where TAttribute : PropertyAttribute
+    {
+#pragma warning disable IDE1006
+        public new TAttribute attribute => base.attribute as TAttribute;
+#pragma warning restore IDE1006
+    }
+}
