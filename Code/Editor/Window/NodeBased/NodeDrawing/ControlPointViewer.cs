26369d97ce74899e1776995dad86cc4a3122a8af
Oleg Pulkin <oleghcp@gmail.com>
2025-06-21 20:45:31 +0300

    update of ControlPointViewer

--- Code/Editor/Window/NodeBased/NodeDrawing/ControlPointViewer.cs
+++ Code/Editor/Window/NodeBased/NodeDrawing/ControlPointViewer.cs
@@ -1,5 +1,4 @@
-﻿using OlegHcp;
-using OlegHcp.Mathematics;
+﻿using OlegHcp.Mathematics;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,9 +6,9 @@ namespace OlegHcpEditor.Window.NodeBased.NodeDrawing
 {
     internal class ControlPointViewer
     {
-        private bool _controlPressed;
         private TransitionViewer _transitionViewer;
         private Vector3[] _trianglePoints = new Vector3[4];
+        private Rect _rect = new Rect(0f, 0f, 8f, 8f);
 
         public ControlPointViewer(TransitionViewer transitionViewer)
         {
@@ -18,64 +17,54 @@ namespace OlegHcpEditor.Window.NodeBased.NodeDrawing
 
         public void DrawSquare(in Vector2 position, in Color color)
         {
-            Handles.color = color;
-            if (Handles.Button(position, Quaternion.identity, 4f, 8f, Handles.DotHandleCap))
-                ButtonAction();
-            Handles.color = Colours.White;
+            CreateRect(position);
+            Handles.DrawSolidRectangleWithOutline(_rect, color, default);
         }
 
         public void DrawTriangle(in Vector2 position, in Quaternion rotation, in Color color)
         {
-            Handles.color = color;
-            if (Handles.Button(position, rotation, 6f, 8f, TriangleHandleCap))
-                ButtonAction();
-            Handles.color = Colours.White;
+            CreateRect(position);
+            DrawTriangleInternal(position, rotation, 6f, color);
         }
 
         public bool HandleEvents(Event e)
         {
-            _controlPressed = e.control;
+            if (e.type != EventType.MouseDown || e.button != 0)
+                return false;
 
-            return false;
-        }
+            if (!_rect.Contains(e.mousePosition))
+                return false;
 
-        private void ButtonAction()
-        {
-            if (Event.current.button != 0)
-                return;
-
-            if (_controlPressed)
+            if (e.control)
                 _transitionViewer.Delete();
             else
                 _transitionViewer.ShowInfoWindow();
+
+            return true;
         }
 
-        private void TriangleHandleCap(int controlID, Vector3 position, Quaternion rotation, float size, EventType eventType)
+        private void CreateRect(in Vector2 position)
         {
-            switch (eventType)
-            {
-                case EventType.MouseMove:
-                case EventType.Layout:
-                    HandleUtility.AddControl(controlID, HandleUtility.DistanceToRectangle(position, rotation, size));
-                    break;
+            _rect.x = position.x - _rect.width * 0.5f;
+            _rect.y = position.y - _rect.height * 0.5f;
+        }
 
-                case EventType.Repaint:
-                    Vector2 pos = position;
-                    float angle = rotation.eulerAngles.z + 90f;
+        private void DrawTriangleInternal(in Vector3 position, in Quaternion rotation, float size, in Color color)
+        {
+            Vector2 pos = position;
+            float angle = rotation.eulerAngles.z + 90f;
 
-                    Vector2 vector0 = MathUtility.AngleToVector2(angle) * size;
-                    Vector2 vector1 = MathUtility.AngleToVector2(angle + 120f) * size;
-                    Vector2 vector2 = MathUtility.AngleToVector2(angle - 120f) * size;
-                    Vector2 lookVector = pos + vector0;
+            Vector2 vector0 = MathUtility.AngleToVector2(angle) * size;
+            Vector2 vector1 = MathUtility.AngleToVector2(angle + 120f) * size;
+            Vector2 vector2 = MathUtility.AngleToVector2(angle - 120f) * size;
+            Vector2 lookVector = pos + vector0;
 
-                    _trianglePoints[0] = pos + vector1;
-                    _trianglePoints[1] = lookVector;
-                    _trianglePoints[2] = lookVector;
-                    _trianglePoints[3] = pos + vector2;
+            _trianglePoints[0] = pos + vector1;
+            _trianglePoints[1] = lookVector;
+            _trianglePoints[2] = lookVector;
+            _trianglePoints[3] = pos + vector2;
 
-                    Handles.DrawSolidRectangleWithOutline(_trianglePoints, Colours.White, default);
-                    break;
-            }
+            Handles.DrawSolidRectangleWithOutline(_trianglePoints, color, default);
         }
     }
 }
