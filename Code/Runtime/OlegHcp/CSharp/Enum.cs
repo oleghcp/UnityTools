1b3ff86f1625208de70ac55fa77b74c8dccf199b
Oleg Pulkin <oleghcp@gmail.com>
2025-07-12 20:16:16 +0300

    update of Enum`1 class

--- Code/Runtime/OlegHcp/CSharp/Enum.cs
+++ Code/Runtime/OlegHcp/CSharp/Enum.cs
@@ -1,14 +1,16 @@
 ﻿using System;
+using System.Reflection;
 
 namespace OlegHcp.CSharp
 {
-    public static class Enum<TEnum> where TEnum : Enum
+    public static class Enum<TEnum> where TEnum : struct, Enum
     {
         public static readonly int Count;
 
         static Enum()
         {
-            Count = Enum.GetNames(typeof(TEnum)).Length;
+            BindingFlags mask = BindingFlags.Static | BindingFlags.Public;
+            Count = typeof(TEnum).GetFields(mask).Length;
         }
     }
 }
