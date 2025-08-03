05ad396c71bba2c21d3908289bc4ca41ad185865
Oleg Pulkin <oleghcp@gmail.com>
2025-08-03 19:13:17 +0300

    update of ComponentUtility

--- Code/Runtime/OlegHcp/ComponentUtility.cs
+++ Code/Runtime/OlegHcp/ComponentUtility.cs
@@ -9,16 +9,6 @@ namespace OlegHcp
         /// Creates and returns an instance of GameObject with Component of T type.
         /// </summary>
         /// <typeparam name="T">Type of component.</typeparam>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static T CreateInstance<T>() where T : Component
-        {
-            return CreateInstance<T>(typeof(T).Name);
-        }
-
-        /// <summary>
-        /// Creates and returns an instance of GameObject with Component of T type.
-        /// </summary>
-        /// <typeparam name="T">Type of component.</typeparam>
         public static T CreateInstance<T>(string gameObjectName) where T : Component
         {
             GameObject go = new GameObject(gameObjectName);
@@ -29,11 +19,10 @@ namespace OlegHcp
         /// Creates and returns an instance of GameObject with Component of T type.
         /// </summary>
         /// <typeparam name="T">Type of component.</typeparam>
-        public static T CreateInstance<T>(Transform parent) where T : Component
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T CreateInstance<T>() where T : Component
         {
-            T component = CreateInstance<T>();
-            component.transform.SetParent(parent);
-            return component;
+            return CreateInstance<T>(typeof(T).Name);
         }
 
         /// <summary>
@@ -44,6 +33,62 @@ namespace OlegHcp
         {
             T component = CreateInstance<T>(gameObjectName);
             component.transform.SetParent(parent);
+            return component;
+        }
+
+        /// <summary>
+        /// Creates and returns an instance of GameObject with Component of T type.
+        /// </summary>
+        /// <typeparam name="T">Type of component.</typeparam>
+        public static T CreateInstance<T>(Transform parent) where T : Component
+        {
+            return CreateInstance<T>(typeof(T).Name, parent);
+        }
+
+        public static T CreateInstance<T>(in Vector3 position) where T : Component
+        {
+            return CreateInstance<T>(typeof(T).Name, position);
+        }
+
+        public static T CreateInstance<T>(in Vector3 position, in Quaternion rotation) where T : Component
+        {
+            return CreateInstance<T>(typeof(T).Name, position, rotation);
+        }
+
+        public static T CreateInstance<T>(Transform parent, in Vector3 position, in Quaternion rotation, bool local) where T : Component
+        {
+            return CreateInstance<T>(typeof(T).Name, parent, position, rotation, local);
+        }
+
+        public static T CreateInstance<T>(string gameObjectName, in Vector3 position) where T : Component
+        {
+            T component = CreateInstance<T>(gameObjectName);
+            component.transform.position = position;
+            return component;
+        }
+
+        public static T CreateInstance<T>(string gameObjectName, in Vector3 position, in Quaternion rotation) where T : Component
+        {
+            T component = CreateInstance<T>(gameObjectName);
+            component.transform.SetPositionAndRotation(position, rotation);
+            return component;
+        }
+
+        public static T CreateInstance<T>(string gameObjectName, Transform parent, in Vector3 position, in Quaternion rotation, bool local) where T : Component
+        {
+            T component = CreateInstance<T>(gameObjectName);
+
+            if (local)
+            {
+                component.transform.SetParent(parent);
+                component.transform.SetLocalPositionAndRotation(position, rotation);
+            }
+            else
+            {
+                component.transform.SetPositionAndRotation(position, rotation);
+                component.transform.SetParent(parent);
+            }
+
             return component;
         }
     }
