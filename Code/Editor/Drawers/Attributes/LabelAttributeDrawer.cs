4634ddb4a7684e6a62118ffc0f3a4a87d14c3aff
Oleg Pulkin <oleghcp@gmail.com>
2025-09-20 20:51:06 +0300

    LabelAttribute

---
+++ Code/Editor/Drawers/Attributes/LabelAttributeDrawer.cs
@@ -0,0 +1,21 @@
+﻿using OlegHcp.Inspector;
+using OlegHcpEditor.Engine;
+using UnityEditor;
+using UnityEngine;
+
+namespace OlegHcpEditor.Drawers.Attributes
+{
+    [CustomPropertyDrawer(typeof(LabelAttribute))]
+    internal class LabelAttributeDrawer : AttributeDrawer<LabelAttribute>
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            property.Draw(position, EditorGuiUtility.TempContent(attribute.Label), true);
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return property.GetHeight(label, true);
+        }
+    }
+}
---
+++ Code/Runtime/OlegHcp/Inspector/LabelAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace OlegHcp.Inspector
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class LabelAttribute : PropertyAttribute
+    {
+        internal readonly string Label;
+
+        public LabelAttribute(string label)
+        {
+            Label = label;
+        }
+    }
+}
